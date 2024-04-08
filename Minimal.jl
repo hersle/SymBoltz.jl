8001d3852e7de99ee9d58423072a448e72839b33
Herman Sletmoen <hermansletmoen@gmail.com>
2024-04-08 10:37:23 +0200

    Added baryons to perturbations

--- Minimal.jl
+++ Minimal.jl
@@ -16,6 +16,7 @@ const α  = PhysicalConstants.CODATA2018.α # fine structure constant, ≈ 1/137
 const me = PhysicalConstants.CODATA2018.m_e / u"kg"
 const mp = PhysicalConstants.CODATA2018.m_p / u"kg"
 const mH = 1.6737236e-27 # kg
+const σT = PhysicalConstants.CODATA2018.σ_e / u"m^2"
 const km  = 1u"km/m"  |> NoUnits
 const pc  = 1u"pc/m"  |> NoUnits
 const Mpc = 1u"Mpc/m" |> NoUnits
@@ -55,6 +56,7 @@ bg_eqs = [
     PΛ ~ -ρΛ
 
     # continuity equation
+    # TODO: replace with analytical solutions
     Da(ρr) ~ -3/a * (ρr + Pr)
     Da(ρm) ~ -3/a * (ρm + Pm)
     Da(ρΛ) ~ -3/a * (ρΛ + PΛ)
@@ -74,8 +76,9 @@ function solve_background(ρr0, ρm0) # TODO: define parameter struct?
 end
 
 # thermodynamics / recombination variables
+# TODO: just merge with background?
 @parameters fb H0 T0
-@variables Xe(a) XH(a) Xp(a) α2(a) β(a) λe(a) ρb(a) nb(a) np(a) ne(a) nH(a) H(a) T(a)
+@variables Xe(a) XH(a) Xp(a) α2(a) β(a) λe(a) ρb(a) nb(a) np(a) ne(a) nH(a) H(a) T(a) dτ(a) R(a)
 thermo_eqs = [
     H ~ E * H0
     T ~ T0 / a # TODO: diff eq for temperature evolution?
@@ -94,6 +97,10 @@ thermo_eqs = [
     XH ~ nH / nb
     Xp ~ np / nb
 
+    # optical depth
+    dτ ~ -ne * σT * c / (a*H) # dτ = dτ/da
+    R ~ 3/4 * ρb/ρr # Dodelson (5.74)
+
     # TODO: reionization?
 ]
 @mtkbuild thermo = ODESystem([bg_eqs; thermo_eqs], a, [ρr, ρm, ρΛ, Xe], [H0, T0, fb])
@@ -109,42 +116,50 @@ end
 
 # perturbation variables
 @parameters k # really k*c/H0
-@variables Φ(a) Ψ(a) δρ(a) Θr0(a) Θr1(a) δm(a) um(a) Δm(a)
+@variables Φ(a) Ψ(a) δρ(a) Θr0(a) Θr1(a) δc(a) δb(a) uc(a) ub(a) ρc(a) Δm(a)
 pert_eqs = [
     # radiation perturbations (density & velocity)
     Da(Θr0) + k/(a^2*E)*Θr1 ~ -Da(Φ) # Dodelson (5.67) or (8.10)
-    Da(Θr1) - k/(3*a^2*E)*Θr0 ~ k/(3*a^2*E)*Ψ # Dodelson (5.67) or (8.11)
+    Da(Θr1) - k/(3*a^2*E)*Θr0 ~ k/(3*a^2*E)*Ψ + dτ * (Θr1 - ub/3) # Dodelson (5.67) or (8.11)
 
     # matter perturbations (density & velocity)
-    Da(δm) + k/(a^2*E)*um ~ -3*Da(Φ) # Dodelson (5.69) or (8.12) with i*uc -> um
-    Da(um) + um/a ~ k/(a^2*E)*Ψ # Dodelson (5.70) or (8.13) with i*uc -> um
+    ρc ~ ρm - ρb
+    Da(δc) + k/(a^2*E)*uc ~ -3*Da(Φ) # Dodelson (5.69) or (8.12) with i*uc -> uc
+    Da(uc) + uc/a ~ k/(a^2*E)*Ψ # Dodelson (5.70) or (8.13) with i*uc -> uc
+
+    # baryon perturbations (density & velocity)
+    Da(δb) + k/(a^2*E)*ub ~ -3*Da(Φ) # Dodelson (5.71) with i*ub -> ub
+    Da(ub) + ub/a ~ k/(a^2*E)*Ψ + dτ/R * (ub - 3*Θr1)# Dodelson (5.72) with i*ub -> ub
 
     # gravity
-    δρ ~ ρm*δm + 4*ρr*Θr0 # total energy density perturbation
+    δρ ~ 4*ρr*Θr0 + δc*ρc + δb*ρb # total energy density perturbation
     Da(Φ) ~ (3/2*a^2*δρ - k^2*Φ - 3*a^2*E^2*Φ) / (3*a^3*E^2) # Dodelson (8.14) # TODO: write in more natural form?
     Δm ~ k^2*Φ / (3/2*a^2*ρm) # gauge-invariant overdensity (from Poisson equation)
 
     # anisotropic stress
     Ψ ~ -Φ # TODO: relax
 ]
-@mtkbuild pert = ODESystem([bg_eqs; pert_eqs], a, [ρr, ρm, ρΛ, Φ, Θr0, δm, Θr1, um], [k])
-pert_prob = ODEProblem(pert, [ρr, ρm, ρΛ, Φ, Θr0, δm, Θr1, um] .=> NaN, (aini, atoday), [k => NaN]; jac=true) # TODO: use remake
+@mtkbuild pert = ODESystem([bg_eqs; thermo_eqs; pert_eqs], a, [ρr, ρm, ρΛ, Xe, Φ, Θr0, δc, δb, Θr1, uc, ub], [H0, k, T0, fb])
+pert_prob = ODEProblem(pert, [ρr, ρm, ρΛ, Xe, Φ, Θr0, δc, δb, Θr1, uc, ub] .=> NaN, (aini, atoday), [H0, k, T0, fb] .=> NaN; jac=true) # TODO: use remake
 
-function solve_perturbations(kval, ρr0, ρm0)
+function solve_perturbations(kval, ρr0, ρm0, ρb0, H0)
+    fb = ρb0 / ρm0; @assert fb <= 1 # TODO: avoid duplication thermo logic
+    T0 = (ρr0 * 15/π^2 * 3*H0^2/(8*π*G) * ħ^3*c^5)^(1/4) / kB
     bg_sol = solve_background(ρr0, ρm0)
     ρrini, ρmini, ρΛini, Eini = bg_sol(aini; idxs = [ρr, ρm, ρΛ, E]) # integrate background from atoday back to aini
+    Xeini = 1 # TODO: avoid duplication thermo logic
     Φini = 1 # arbitrary normalization (from primordial curvature power spectrum?)
     Θr0ini = Φini/2 # Dodelson (7.89)
-    δmini = 3*Θr0ini # Dodelson (7.94)
+    δcini = δbini = 3*Θr0ini # Dodelson (7.94)
     Θr1ini = -kval*Φini/(6*aini*Eini) # Dodelson (7.95) # TODO: replace aini -> a when this is fixed? https://github.com/SciML/ModelingToolkit.jl/issues/2543
-    umini = 3*Θr1ini # Dodelson (7.95)
-    prob = remake(pert_prob; u0=[ρrini, ρmini, ρΛini, Φini, Θr0ini, Θr1ini, δmini, umini], p=[kval]) # TODO: guarantee order!!
-    return solve(prob, KenCarp4(), reltol=1e-5) # KenCarp4 and Kvaerno5 works well # TODO: use different EnsembleAlgorithm https://docs.sciml.ai/DiffEqDocs/stable/solvers/ode_solve/#Stiff-Problems
+    ucini = ubini = 3*Θr1ini # Dodelson (7.95)
+    prob = remake(pert_prob; u0=[ρrini, ρmini, ρΛini, Xeini, Φini, Θr0ini, Θr1ini, δcini, ucini, δbini, ubini], p=[H0, kval, T0, fb]) # order checked with SymbolicIndexingInterface.parameter_index TODO: guarantee order!!
+    return solve(prob, KenCarp4(), reltol=1e-10) # KenCarp4 and Kvaerno5 works well # TODO: use different EnsembleAlgorithm https://docs.sciml.ai/DiffEqDocs/stable/solvers/ode_solve/#Stiff-Problems
 end
 
 # power spectra
 P0(k, As) = As / k^3
-P(k, ρr0, ρm0, As) = P0(k, As) * solve_perturbations(k, ρr0, ρm0)(atoday; idxs=Δm)^2
+P(k, ρr0, ρm0, ρb0, H0, As) = P0(k, As) * solve_perturbations(k, ρr0, ρm0, ρb0, H0)(atoday; idxs=Δm)^2
 
 if true
     ρr0 = 1e-5
@@ -158,7 +173,7 @@ if true
 
     bg_sol = solve_background(ρr0, ρm0)
     thermo_sol = solve_thermodynamics(ρr0, ρm0, ρb0, H0)
-    pert_sols = [solve_perturbations(kval, ρr0, ρm0) for kval in ks] # TODO: use EnsembleProblem again
+    pert_sols = [solve_perturbations(kval, ρr0, ρm0, ρb0, H0) for kval in ks] # TODO: use EnsembleProblem again
 
     # TODO: add plot recipe!
     p = plot(layout=(3,3), size=(1200, 900), margin=20*Plots.px); display(p)
@@ -167,12 +182,12 @@ if true
     plot!(p[2], log10.(as), log10.(bg_sol.(as; idxs=E) / bg_sol(atoday; idxs=E)); xlabel="lg(a)", ylabel="lg(H/H0)"); display(p)
     plot!(p[3], log10.(as), log10.(thermo_sol.(as, idxs=Xe)); xlabel="lg(a)", ylabel="lg(Xe)"); display(p)
     plot!(p[4], log10.(as), [pert_sol.(as; idxs=Φ) for pert_sol in pert_sols]; xlabel="lg(a)", ylabel="Φ"); display(p)
-    plot!(p[5], log10.(as), [log10.(pert_sol.(as; idxs=δm)) for pert_sol in pert_sols]; xlabel="lg(a)", ylabel="lg(δm)"); display(p)
-    
+    plot!(p[5], log10.(as), [[log10.(abs.(pert_sol.(as; idxs=δ))) for pert_sol in pert_sols] for δ in [δb,δc]]; color=[(1:length(ks))' (1:length(ks))'], xlabel="lg(a)", ylabel="lg(|δb|), lg(δc)"); display(p)
+
     # compute derivatives of output power spectrum with respect to input parameters
     derivatives = [FiniteDiff.finite_difference_derivative, ForwardDiff.derivative]
-    plot!(p[6], log10.(ks*k0), [[derivative.(lgk -> log10(P(10^lgk, ρr0, ρm0, As)), log10.(ks)) for derivative in derivatives]..., log10.(P.(ks, ρr0, ρm0, As)/k0^3)]; xlabel="lg(k/(h/Mpc))", label=["d lg(P) / d lg(k) (auto. diff.)" "d lg(P) / d lg(k) (fin. diff.)" "lg(P/(Mpc/h)³)"], legend=:bottomleft); display(p)
-    plot!(p[7], log10.(ks*k0), [derivative(lgρr0 -> log10.(P.(ks, 10^lgρr0, ρm0, As)), log10(ρr0)) for derivative in derivatives]; xlabel="lg(k/(h/Mpc))", ylabel="d lg(P) / d lg(Ωr0)", labels=["fin. diff." "auto. diff."]); display(p)
-    plot!(p[8], log10.(ks*k0), [derivative(lgρm0 -> log10.(P.(ks, ρr0, 10^lgρm0, As)), log10(ρm0)) for derivative in derivatives]; xlabel="lg(k/(h/Mpc))", ylabel="d lg(P) / d lg(Ωm0)", labels=["fin. diff." "auto. diff."]); display(p)
-    plot!(p[9], log10.(ks*k0), [derivative(lgAs  -> log10.(P.(ks, ρr0, ρm0, 10^lgAs)), log10(As)) for derivative in derivatives]; xlabel="lg(k/(h/Mpc))", ylabel="d lg(P) / d lg(As)", labels=["fin. diff." "auto. diff."], ylims=(0, 2)); display(p)
+    plot!(p[6], log10.(ks*k0), [[derivative.(lgk -> log10(P(10^lgk, ρr0, ρm0, ρb0, H0, As)), log10.(ks)) for derivative in derivatives]..., log10.(P.(ks, ρr0, ρm0, ρb0, H0, As)/k0^3)]; xlabel="lg(k/(h/Mpc))", label=["d lg(P) / d lg(k) (auto. diff.)" "d lg(P) / d lg(k) (fin. diff.)" "lg(P/(Mpc/h)³)"], legend=:bottomleft); display(p)
+    plot!(p[7], log10.(ks*k0), [derivative(lgρr0 -> log10.(P.(ks, 10^lgρr0, ρm0, ρb0, H0, As)), log10(ρr0)) for derivative in derivatives]; xlabel="lg(k/(h/Mpc))", ylabel="d lg(P) / d lg(Ωr0)", labels=["fin. diff." "auto. diff."]); display(p)
+    plot!(p[8], log10.(ks*k0), [derivative(lgρm0 -> log10.(P.(ks, ρr0, 10^lgρm0, ρb0, H0, As)), log10(ρm0)) for derivative in derivatives]; xlabel="lg(k/(h/Mpc))", ylabel="d lg(P) / d lg(Ωm0)", labels=["fin. diff." "auto. diff."]); display(p)
+    plot!(p[9], log10.(ks*k0), [derivative(lgAs  -> log10.(P.(ks, ρr0, ρm0, ρb0, H0, 10^lgAs)), log10(As)) for derivative in derivatives]; xlabel="lg(k/(h/Mpc))", ylabel="d lg(P) / d lg(As)", labels=["fin. diff." "auto. diff."], ylims=(0, 2)); display(p)
 end
