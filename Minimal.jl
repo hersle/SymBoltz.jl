cd2b7ef609c43d3c1553bae9d5ffc58f23b6e3d2
Herman Sletmoen <hermansletmoen@gmail.com>
2024-04-09 12:21:57 +0200

    Compose background, thermodynamics and perturbations

--- Minimal.jl
+++ Minimal.jl
@@ -36,7 +36,7 @@ const EHion = 13.59844 * eV
 # TODO: modified gravity: (coupled) quintessence, Brans-Dicke, DGP, parametrized framework, EFT of LSS, ...
 # TODO: analytical solutions of e.g. background ρ evolution
 # TODO: GPU-parallellized EnsembleProblem
-# TODO: use b = ln(a) as independent variable?
+# TODO: relate parameters through parameter expressions: https://docs.sciml.ai/ModelingToolkit/stable/basics/Composition/#Variable-scope-and-parameter-expressions
 
 # independent variable: scale factor
 @variables b
@@ -44,33 +44,39 @@ Db = Differential(b)
 aini, atoday = 1e-8, 1e0
 bini, btoday = log(aini), log(atoday)
 
-# background variables
-@variables a(b) E(b) ρ(b) ρr(b) ρm(b) ρΛ(b) Pr(b) Pm(b) PΛ(b) Ωr(b) Ωm(b) ΩΛ(b)
-bg_eqs = [
-    a ~ exp(b) # b = ln(a) # TODO: should check more carefully that reparametrization from a to b is correct
+function background_gravity_GR(; name)
+    @variables E(b) ρ(b)
+    return ODESystem([
+        E ~ √(ρ) # H/H₀
+    ], b; name)
+end
 
-    # Friedmann equation
-    ρ ~ ρr + ρm + ρΛ # energy densities and pressures are in units of 3H₀²c² / 8πG
-    E ~ √(ρ) # H/H₀
+function background_species_constant_eos(w; name)
+    @variables ρ(b) P(b) Ω(b) ρcrit(b)
+    return ODESystem([
+        P ~ w*ρ
+        Db(ρ) ~ -3 * (ρ + P) # TODO: replace with analytical solutions
+        Ω ~ ρ / ρcrit
+    ], b; name)
+end
 
-    # equations of state
-    Pr ~ ρr/3
-    Pm ~ 0
-    PΛ ~ -ρΛ
+@named rad = background_species_constant_eos(1//3)
+@named mat = background_species_constant_eos(0)
+@named de = background_species_constant_eos(-1)
+@named grav = background_gravity_GR()
 
-    # continuity equation
-    # TODO: replace with analytical solutions
-    Db(ρr) ~ -3 * (ρr + Pr)
-    Db(ρm) ~ -3 * (ρm + Pm)
-    Db(ρΛ) ~ -3 * (ρΛ + PΛ)
+@variables a(b)
+@named bg = ODESystem([
+    a ~ exp(b)
+    grav.ρ ~ rad.ρ + mat.ρ + de.ρ
+    rad.ρcrit ~ grav.ρ
+    mat.ρcrit ~ grav.ρ
+    de.ρcrit ~ grav.ρ
+], b)
 
-    # dimensionless density parameters
-    Ωr ~ ρr / ρ
-    Ωm ~ ρm / ρ
-    ΩΛ ~ ρΛ / ρ
-]
-@mtkbuild bg = ODESystem(bg_eqs, b, [ρr, ρm, ρΛ], [])
-bg_prob = ODEProblem(bg, [ρr, ρm, ρΛ] .=> NaN, (btoday, bini), []) # TODO: integrate from aini; make a "shooting interface" for different parameters
+@named bg = compose(bg, rad, mat, de, grav)
+bg_simple = structural_simplify(bg)
+bg_prob = ODEProblem(bg_simple, [rad.ρ, mat.ρ, de.ρ] .=> NaN, (btoday, bini), [])
 function solve_background(ρr0, ρm0)
     ρΛ0 = 1 - ρr0 - ρm0
     prob = remake(bg_prob; u0=[ρr0, ρm0, ρΛ0]) # https://github.com/SciML/SymbolicIndexingInterface.jl/issues/59
@@ -81,8 +87,8 @@ end
 # TODO: just merge with background?
 @parameters fb H0 T0
 @variables Xe(b) XH(b) Xp(b) α2(b) β(b) λe(b) ρb(b) nb(b) np(b) ne(b) nH(b) H(b) T(b) dτ(b) R(b)
-thermo_eqs = [
-    H ~ E * H0
+@named th = ODESystem([
+    H ~ grav.E * H0
     T ~ T0 / a # TODO: diff eq for temperature evolution?
 
     # TODO: add Peebles' corrections & He? see Dodelson exercise 4.7
@@ -91,7 +97,7 @@ thermo_eqs = [
     β ~ α2 / λe^3 * exp(-EHion/(kB*T)) # Dodelson (4.37)-(4.38) (γ + H → e⁻ + p)
     λe ~ h / √(2π*me*kB*T) # electron de-Broglie wavelength
 
-    ρb ~ fb * ρm # fb is baryon-to-matter fraction
+    ρb ~ fb * mat.ρ # fb is baryon-to-matter fraction
     nb ~ ρb * 3*H0^2 / (8π*G) / (Xe*mp + (1-Xe)*mH) # ≈ ρb/mp * 3*H0^2 / (8π*G), Dodelson above (4.41) (ρb = ρp+ρH, nb=ρp+ρH)
     np ~ ne # charge neutrality
     nH ~ nb - ne # nb = nH + ne = nH + np
@@ -99,62 +105,64 @@ thermo_eqs = [
 
     # optical depth
     dτ ~ -ne * σT * c / H # dτ = dτ/db
-    R ~ 3/4 * ρb/ρr # Dodelson (5.74)
+    R ~ 3/4 * ρb/rad.ρ # Dodelson (5.74)
 
     # TODO: reionization?
-]
-@mtkbuild thermo = ODESystem([bg_eqs; thermo_eqs], b, [ρr, ρm, ρΛ, Xe], [H0, T0, fb])
-thermo_prob = ODEProblem(thermo, [ρr, ρm, ρΛ, Xe] .=> NaN, (bini, btoday), [H0, T0, fb] .=> NaN)
+], b)
+th = extend(th, bg)
+th_simple = structural_simplify(th)
+th_prob = ODEProblem(th_simple, [Xe, rad.ρ, mat.ρ, de.ρ] .=> NaN, (bini, btoday), [th.H0, th.T0, th.fb] .=> NaN; jac=true)
 function solve_thermodynamics(ρr0, ρm0, ρb0, H0)
     fb = ρb0 / ρm0; @assert fb <= 1
     T0 = (ρr0 * 15/π^2 * 3*H0^2/(8*π*G) * ħ^3*c^5)^(1/4) / kB
     bg_sol = solve_background(ρr0, ρm0)
-    ρrini, ρmini, ρΛini = bg_sol(bini; idxs = [ρr, ρm, ρΛ]) # integrate background from atoday back to aini
-    prob = remake(thermo_prob; u0=[ρrini, ρmini, ρΛini, 1], p=[H0, T0, fb]) # TODO: guarantee order!!
+    ρrini, ρmini, ρΛini = bg_sol(bini; idxs = [rad.ρ, mat.ρ, de.ρ]) # integrate background from atoday back to aini
+    prob = remake(th_prob; u0=[1, ρrini, ρmini, ρΛini], p=[H0, T0, fb]) # TODO: guarantee order!!
     return solve(prob, KenCarp4(), reltol=1e-15) # TODO: after switching ivar from a to b=ln(a), the integrator needs more steps. fix this?
 end
 
 # perturbation variables
 @parameters k # really k*c/H0
 @variables Φ(b) Ψ(b) δρ(b) Θr0(b) Θr1(b) δc(b) δb(b) uc(b) ub(b) ρc(b) Δm(b)
-pert_eqs = [
+@named pt = ODESystem([
     # radiation perturbations (density & velocity)
-    Db(Θr0) + k/(a*E)*Θr1 ~ -Db(Φ) # Dodelson (5.67) or (8.10)
-    Db(Θr1) - k/(3*a*E)*Θr0 ~ k/(3*a*E)*Ψ + dτ * (Θr1 - ub/3) # Dodelson (5.67) or (8.11)
+    Db(Θr0) + k/(a*grav.E)*Θr1 ~ -Db(Φ) # Dodelson (5.67) or (8.10)
+    Db(Θr1) - k/(3*a*grav.E)*Θr0 ~ k/(3*a*grav.E)*Ψ + dτ * (Θr1 - ub/3) # Dodelson (5.67) or (8.11)
 
     # matter perturbations (density & velocity)
-    ρc ~ ρm - ρb
-    Db(δc) + k/(a*E)*uc ~ -3*Db(Φ) # Dodelson (5.69) or (8.12) with i*uc -> uc
-    Db(uc) + uc ~ k/(a*E)*Ψ # Dodelson (5.70) or (8.13) with i*uc -> uc
+    ρc ~ mat.ρ - ρb
+    Db(δc) + k/(a*grav.E)*uc ~ -3*Db(Φ) # Dodelson (5.69) or (8.12) with i*uc -> uc
+    Db(uc) + uc ~ k/(a*grav.E)*Ψ # Dodelson (5.70) or (8.13) with i*uc -> uc
 
     # baryon perturbations (density & velocity)
-    Db(δb) + k/(a*E)*ub ~ -3*Db(Φ) # Dodelson (5.71) with i*ub -> ub
-    Db(ub) + ub ~ k/(a*E)*Ψ + dτ/R * (ub - 3*Θr1)# Dodelson (5.72) with i*ub -> ub
+    Db(δb) + k/(a*grav.E)*ub ~ -3*Db(Φ) # Dodelson (5.71) with i*ub -> ub
+    Db(ub) + ub ~ k/(a*grav.E)*Ψ + dτ/R * (ub - 3*Θr1)# Dodelson (5.72) with i*ub -> ub
 
     # gravity
-    δρ ~ 4*ρr*Θr0 + δc*ρc + δb*ρb # total energy density perturbation
-    Db(Φ) ~ (3/2*a^2*δρ - k^2*Φ - 3*(a*E)^2*Φ) / (3*(a*E)^2) # Dodelson (8.14) # TODO: write in more natural form?
-    Δm ~ k^2*Φ / (3/2*a^2*ρm) # gauge-invariant overdensity (from Poisson equation)
+    δρ ~ 4*rad.ρ*Θr0 + δc*ρc + δb*ρb # total energy density perturbation
+    Db(Φ) ~ (3/2*a^2*δρ - k^2*Φ - 3*(a*grav.E)^2*Φ) / (3*(a*grav.E)^2) # Dodelson (8.14) # TODO: write in more natural form?
+    Δm ~ k^2*Φ / (3/2*a^2*mat.ρ) # gauge-invariant overdensity (from Poisson equation)
 
     # anisotropic stress
     Ψ ~ -Φ # TODO: relax
-]
-@mtkbuild pert = ODESystem([bg_eqs; thermo_eqs; pert_eqs], b, [ρr, ρm, ρΛ, Xe, Φ, Θr0, δc, δb, Θr1, uc, ub], [H0, k, T0, fb])
-pert_prob = ODEProblem(pert, [ρr, ρm, ρΛ, Xe, Φ, Θr0, δc, δb, Θr1, uc, ub] .=> NaN, (bini, btoday), [H0, k, T0, fb] .=> NaN; jac=true) # TODO: use remake
+], b)
+pt = extend(pt, th)
+pt_simple = structural_simplify(pt)
+pt_prob = ODEProblem(pt_simple, [Xe, Φ, Θr0, Θr1, δc, uc, δb, ub, rad.ρ, mat.ρ, de.ρ] .=> NaN, (bini, btoday), [pt_simple.H0, pt_simple.k, pt_simple.T0, pt_simple.fb] .=> NaN; jac=true) 
 
 function solve_perturbations(kval, ρr0, ρm0, ρb0, H0)
     println("k = $(kval*k0) Mpc/h")
     fb = ρb0 / ρm0; @assert fb <= 1 # TODO: avoid duplication thermo logic
     T0 = (ρr0 * 15/π^2 * 3*H0^2/(8*π*G) * ħ^3*c^5)^(1/4) / kB
     bg_sol = solve_background(ρr0, ρm0)
-    ρrini, ρmini, ρΛini, Eini = bg_sol(bini; idxs = [ρr, ρm, ρΛ, E]) # integrate background from atoday back to aini
-    Xeini = 1 # TODO: avoid duplication thermo logic
-    Φini = 1 # arbitrary normalization (from primordial curvature power spectrum?)
+    ρrini, ρmini, ρΛini, Eini = bg_sol(bini; idxs = [rad.ρ, mat.ρ, de.ρ, grav.E]) # integrate background from atoday back to aini
+    Xeini = 1.0 # TODO: avoid duplication thermo logic
+    Φini = 1.0 # arbitrary normalization (from primordial curvature power spectrum?)
     Θr0ini = Φini/2 # Dodelson (7.89)
     δcini = δbini = 3*Θr0ini # Dodelson (7.94)
     Θr1ini = -kval*Φini/(6*aini*Eini) # Dodelson (7.95) # TODO: replace aini -> a when this is fixed? https://github.com/SciML/ModelingToolkit.jl/issues/2543
     ucini = ubini = 3*Θr1ini # Dodelson (7.95)
-    prob = remake(pert_prob; u0=[ρrini, ρmini, ρΛini, Xeini, Φini, Θr0ini, Θr1ini, δcini, ucini, δbini, ubini], p=[H0, kval, T0, fb]) # order checked with SymbolicIndexingInterface.parameter_index TODO: guarantee order!!
+    prob = remake(pt_prob; u0=[Xeini, Φini, Θr0ini, Θr1ini, δcini, ucini, δbini, ubini, ρrini, ρmini, ρΛini], p=[H0, kval, T0, fb]) # order checked with SymbolicIndexingInterface.parameter_index (on the ODEProblem!!!) TODO: guarantee order!!
     return solve(prob, KenCarp4(), reltol=1e-10) # KenCarp4 and Kvaerno5 works well # TODO: use different EnsembleAlgorithm https://docs.sciml.ai/DiffEqDocs/stable/solvers/ode_solve/#Stiff-Problems
 end
 
@@ -178,15 +186,15 @@ if true
     p = plot(layout=(3,3), size=(1200, 900), margin=20*Plots.px); display(p)
 
     bg_sol = solve_background(ρr0, ρm0)
-    plot!(p[1], log10.(as), reduce(vcat, bg_sol.(bs; idxs=[Ωr,Ωm,ΩΛ])'); xlabel="lg(a)", ylabel="Ω", label=["Ωr" "Ωm" "ΩΛ"], legend=:left); display(p)
-    plot!(p[2], log10.(as), log10.(bg_sol.(bs; idxs=E) / bg_sol(btoday; idxs=E)); xlabel="lg(a)", ylabel="lg(H/H0)"); display(p)
+    plot!(p[1], log10.(as), reduce(vcat, bg_sol.(bs; idxs=[rad.Ω,mat.Ω,de.Ω])'); xlabel="lg(a)", ylabel="Ω", label=["Ωr" "Ωm" "ΩΛ"], legend=:left); display(p)
+    plot!(p[2], log10.(as), log10.(bg_sol.(bs; idxs=grav.E) / bg_sol(btoday; idxs=grav.E)); xlabel="lg(a)", ylabel="lg(H/H0)"); display(p)
 
-    thermo_sol = solve_thermodynamics(ρr0, ρm0, ρb0, H0)
-    plot!(p[3], log10.(as), log10.(thermo_sol.(bs, idxs=Xe)); xlabel="lg(a)", ylabel="lg(Xe)"); display(p)
-    
-    pert_sols = [solve_perturbations(kval, ρr0, ρm0, ρb0, H0) for kval in ks] # TODO: use EnsembleProblem again
-    plot!(p[4], log10.(as), [pert_sol.(bs; idxs=Φ) for pert_sol in pert_sols]; xlabel="lg(a)", ylabel="Φ"); display(p)
-    plot!(p[5], log10.(as), [[log10.(abs.(pert_sol.(bs; idxs=δ))) for pert_sol in pert_sols] for δ in [δb,δc]]; color=[(1:length(ks))' (1:length(ks))'], xlabel="lg(a)", ylabel="lg(|δb|), lg(δc)"); display(p)
+    th_sol = solve_thermodynamics(ρr0, ρm0, ρb0, H0)
+    plot!(p[3], log10.(as), log10.(th_sol.(bs, idxs=th.Xe)); xlabel="lg(a)", ylabel="lg(Xe)"); display(p)
+
+    pt_sols = [solve_perturbations(kval, ρr0, ρm0, ρb0, H0) for kval in ks] # TODO: use EnsembleProblem again
+    plot!(p[4], log10.(as), [pt_sol.(bs; idxs=Φ) for pt_sol in pt_sols]; xlabel="lg(a)", ylabel="Φ"); display(p)
+    plot!(p[5], log10.(as), [[log10.(abs.(pt_sol.(bs; idxs=δ))) for pt_sol in pt_sols] for δ in [δb,δc]]; color=[(1:length(ks))' (1:length(ks))'], xlabel="lg(a)", ylabel="lg(|δb|), lg(δc)"); display(p)
 
     x0 = [log10(ρr0), log10(ρm0), log10(ρb0), log10(H0), log10(As)]
     Ps = stack([P([log10(k), x0...]) for k in ks]) # TODO: use pert_sols
