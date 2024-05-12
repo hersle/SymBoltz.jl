e75a768d5c755413b0b5790b4ef3a8eed13cb394
Herman Sletmoen <hermansletmoen@gmail.com>
2024-05-12 12:58:05 +0200

    Reorganize CMB demo

--- Spectra.jl
+++ Spectra.jl
@@ -1,5 +1,6 @@
 using NumericalIntegration
 using Bessels: besselj, sphericalbesselj
+using ForwardDiff
 
 # matter power spectrum
 P0(k, As) = @. As / k ^ 3
@@ -8,3 +9,123 @@ function P(pt::PerturbationsSystem, k, Ωr0, Ωm0, Ωb0, h, As, Yp)
     ηtoday = pt_sols[1].prob.tspan[end] # TODO: something more robust?
     return P0(k, As) .* pt_sols(ηtoday, idxs=pt.sys.gravpt.Δm) .^ 2
 end
+
+range_until(start, stop, step; skip_start=false) = range(skip_start ? start+step : start, step=step, length=Int(ceil((stop-start)/step+1)))
+# TODO: only need as from a = 1e-4 till today
+function S(pt::PerturbationsSystem, ηs::AbstractArray, ks::AbstractArray, Ωr0, Ωm0, Ωb0, h, Yp; Sspline_ks=nothing)    
+    Ss = zeros(eltype([Ωr0, Ωm0, Ωb0, h, Yp]), (length(ηs), length(ks)))
+
+    if !isnothing(Sspline_ks)
+        Ssplinedata = S(pt, ηs, Sspline_ks, Ωr0, Ωm0, Ωb0, h, Yp)
+        for i_η in eachindex(ηs)
+            Sspline = CubicSpline(Ssplinedata[i_η,:], Sspline_ks)
+            Ss[i_η,:] .= Sspline(ks)
+        end
+        return Ss
+    end
+
+    th = pt.th
+    th_sol = solve(th, Ωr0, Ωm0, Ωb0, h, Yp)
+    η0 = th_sol.t[end]
+    τ(η) = th_sol(η, idxs=th.sys.τ).u .- th_sol(η0, idxs=th.sys.τ)
+    τ′(η) = th_sol(η, Val{1}, idxs=th.sys.τ).u
+    τ′′(η) = th_sol(η, Val{2}, idxs=th.sys.τ).u
+    g(η) = .-τ′(η) .* exp.(.-τ(η))
+    g′(η) = (τ′(η) .^ 2 - τ′′(η)) .* exp.(.-τ(η))
+    
+    # TODO: use saveat for ηs
+    pt_sols = solve(pt, ks, Ωr0, Ωm0, Ωb0, h, Yp)
+    for (i_k, (k, pt_sol)) in enumerate(zip(ks, pt_sols))
+        # TODO: must be faster!! use saveat for ηs in ODESolution?
+        # TODO: add source functions as observed perturbation functions? but difficult with cumulative τ(η)? must anyway wait for this to be fixed: https://github.com/SciML/ModelingToolkit.jl/issues/2697
+        Θ0(η) = pt_sol(η, idxs=pt.sys.ph.Θ0)
+        Ψ(η) = pt_sol(η, idxs=pt.sys.gravpt.Ψ)
+        Φ(η) = pt_sol(η, idxs=pt.sys.gravpt.Φ)
+        Π(η) = pt_sol(η, idxs=pt.sys.pol.Π)
+        ub(η) = pt_sol(η, idxs=pt.sys.bar.u)
+        function Ψ′(η)
+            # TODO: use pt_sol(...) when this is fixed: https://github.com/SciML/ModelingToolkit.jl/issues/2697 and https://github.com/SciML/ModelingToolkit.jl/pull/2574
+            #return pt_sol(η, Val{1}, idxs=pt.Ψ).u
+            # workaround: spline and take derivative
+            # TODO: differentiate Ψ = ... expression and calculate rhs from actual states?
+            Ψspl = CubicSpline(Ψ(η).u, η; extrapolate=true)
+            return DataInterpolations.derivative.(Ref(Ψspl), η)
+        end
+        Φ′(η) = pt_sol(η, Val{1}, idxs=pt.sys.gravpt.Φ).u
+        ub′(η) = pt_sol(η, Val{1}, idxs=pt.sys.bar.u).u
+
+        Ss[:,i_k] .+= g(ηs) .* (Θ0(ηs) + Ψ(ηs) + Π(ηs)/4) # SW
+        Ss[:,i_k] .+= (g′(ηs).*ub(ηs) + g(ηs).*ub′(ηs)) / k # Doppler
+        Ss[:,i_k] .+= exp.(.-τ(ηs)) .* (Ψ′(ηs) - Φ′(ηs)) # ISW
+        # TODO: add polarization
+    end
+
+    return Ss
+end
+
+#Ss = S(ηs, ks, Ωr0, Ωm0, Ωb0, h, Yp)
+#plot(ηs, asinh.(Ss[:,[1,9]]))
+
+# TODO: contribute back to Bessels.jl
+sphericalbesseljslow(ls::AbstractRange, x) = sphericalbesselj.(ls, x)
+sphericalbesseljfast(ls::AbstractRange, x) = (x == 0.0 ? 1.0 : √(π/(2*x))) * besselj(ls .+ 0.5, x)
+
+# TODO: integrate CubicSplines instead of trapz! https://discourse.julialang.org/t/how-to-speed-up-the-numerical-integration-with-interpolation/96223/5
+function Cl(pt::PerturbationsSystem, ls::AbstractArray, ks::AbstractRange, lnηs::AbstractRange, Ωr0, Ωm0, Ωb0, h, As, Yp; kwargs...)
+    ηs = exp.(lnηs)
+    Ss = S(pt, ηs, ks, Ωr0, Ωm0, Ωb0, h, Yp; kwargs...) # TODO: reduce memory allocation!
+    η0 = ηs[end]
+
+    T = eltype([Ωr0, Ωm0, Ωb0, h, As, Yp]) # TODO: handle with/without As differently?
+    ∂Θ_∂lnη = zeros(T, (length(ls), length(ηs)))
+    Θls = zeros(T, (length(ls), length(ks)))
+    ks_with0 = [0.0; ks]
+    dCl_dks_with0 = zeros(T, length(ks_with0))
+    Cls = zeros(T, length(ls))
+
+    ls_all = minimum(ls):1:maximum(ls) # range with step 1
+    ls_indices = 1 .+ ls .- ls_all[1] # indices such that ls = ls_all[ls_indices]
+    Jls = zeros(length(ls))
+
+    for (i_k, k) in enumerate(ks)
+        for (i_η, η) in enumerate(ηs)
+            # TODO: try to spline sphericalbesselj for each l, from x=0 to x=kmax*(η0-ηini)
+            Jls .= sphericalbesseljfast(ls_all, k*(η0-η))[ls_indices] # @btime reference (~1000 ls): 5.4 s
+            #Jls .= sphericalbesselj.(ls, k*(η0-η)) # @btime reference (~1000 ls): 32 s
+            ∂Θ_∂lnη[:, i_η] .= Ss[i_η, i_k] * Jls * η # TODO: sphericalbesselj takes a lot of time!
+        end
+        for (i_l, l) in enumerate(ls)
+            Θls[i_l, i_k] = integrate(lnηs, ∂Θ_∂lnη[i_l, :], SimpsonEven()) # integrate over η # TODO: add starting Θl(ηini) # TODO: calculate ∂Θ_∂logΘ and use Even() methods
+        end
+    end
+
+    for (i_l, l) in enumerate(ls)
+        dCl_dks_with0[2:end] .= @. 2/π * ks^2 * P0(ks, As) * Θls[i_l, :]^2
+        Cls[i_l] = integrate(ks_with0, dCl_dks_with0, SimpsonEven()) # integrate over k (_with0 adds one additional point at (0,0))
+    end
+
+    return Cls
+end
+
+function Cl(pt::PerturbationsSystem, ls::AbstractArray, Ωr0, Ωm0, Ωb0, h, As, Yp; splineS = true)
+    bg_sol = solve(pt.bg, Ωr0, Ωm0)
+
+    ηi, η0 = bg_sol[η][begin], bg_sol[η][end]
+    ηi, η0 = ForwardDiff.value.([ηi, η0]) # TODO: do I lose some gradient information here?!
+    lnηs = range(log(ηi), log(η0), length=400) # logarithmic spread to capture early-time oscillations # TODO: dynamic/adaptive spacing!
+
+    kmax = 2.0 * ls[end]
+    ks = range_until(0, kmax, 2π/4; skip_start=true) ./ η0
+    if splineS
+        Sspline_ks = range_until(0, kmax, 50; skip_start=true) ./ η0 # Δk = 50/η0
+    else
+        Sspline_ks = nothing
+    end
+
+    return Cl(pt, ls, ks, lnηs, Ωr0, Ωm0, Ωb0, h, As, Yp; Sspline_ks)
+end
+
+Dl(pt::PerturbationsSystem, ls::AbstractArray, args...; kwargs...) = Cl(pt, ls, args...; kwargs...) .* ls .* (ls .+ 1) / (2*π)
+
+#Dls = Dl(ls, ks, ηs, Ωr0, Ωm0, Ωb0, h, As, Yp; Sspline_ks)
+#plot(ls, Dls; xlabel="l", ylabel="Dl = l (l+1) Cl / 2π")
---
+++ test/cmbspectrum.jl
@@ -0,0 +1,53 @@
+using .Symboltz
+using .Symboltz: η
+using ModelingToolkit
+using Plots; Plots.default(label=nothing)
+using ForwardDiff, DiffResults, FiniteDiff
+
+@kwdef struct Parameters
+    Ωr0 = 5.5e-5
+    Ωm0 = 0.317
+    Ωb0 = 0.05
+    h = 0.67
+    As = 2.1e-9
+    Yp = 0.245
+end
+
+par = Parameters()
+
+@named g = Symboltz.background_metric()
+@named grav = Symboltz.background_gravity_GR(g)
+@named rad = Symboltz.background_radiation(g)
+@named mat = Symboltz.background_matter(g)
+@named de = Symboltz.background_cosmological_constant(g)
+@named bg = BackgroundSystem(g, grav, [rad, mat, de])
+
+@named temp = Symboltz.thermodynamics_temperature(g)
+@named Herec = Symboltz.recombination_helium_saha()
+@named Hrec = Symboltz.recombination_hydrogen_peebles(g)
+@named reion1 = Symboltz.reionization_smooth_step(g, 8.0, 0.5, 1 + Herec.Yp/(4*(1-Herec.Yp))) # TODO: separate reionH₊, reionHe₊, reionHe₊₊
+@named reion2 = Symboltz.reionization_smooth_step(g, 3.5, 0.5, Herec.Yp/(4*(1-Herec.Yp)))
+@named th = ThermodynamicsSystem(bg, Herec, Hrec, temp, [reion1, reion2])
+
+lmax = 6
+@named gpt = Symboltz.perturbations_metric()
+@named ph = Symboltz.perturbations_photon_hierarchy(gpt, lmax, true)
+@named pol = Symboltz.perturbations_polarization_hierarchy(gpt, lmax)
+@named cdm = Symboltz.perturbations_matter(g, gpt, false)
+@named bar = Symboltz.perturbations_matter(g, gpt, true)
+@named gravpt = Symboltz.perturbations_gravity(g, gpt, )
+@named pt = PerturbationsSystem(bg, th, gpt, gravpt, ph, pol, cdm, bar)
+
+ls = [2:1:8; 10; 12; 16; 22; 30:10:2500]
+θ0 = [par.Ωr0, par.Ωm0, par.Ωb0, par.h, par.As, par.Yp]
+
+# differentiated CMB power spectrum
+lgDl(lgθ) = log10.(Symboltz.Dl(pt, ls, (10 .^ lgθ)...))
+lgDlres = DiffResults.JacobianResult(Float64.(ls), θ0)
+ForwardDiff.jacobian!(lgDlres, lgDl, log10.(θ0))
+lgDls, dlgDl_dθs_ad = DiffResults.value(lgDlres), DiffResults.jacobian(lgDlres)
+Dls = 10 .^ lgDls
+
+p = plot(layout=(2,1), size=(800, 1000), left_margin=bottom_margin=30*Plots.px); display(p)
+plot!(p[1], ls, Dls / 1e-12; xlabel = "l", ylabel = "Dₗ=l (l+1) Cₗ / 2π / 10⁻¹²", title = "CMB power spectrum"); display(p)
+plot!(p[2], ls, dlgDl_dθs_ad; xlabel = "l", ylabel = "∂ lg(Dₗ) / ∂ lg(θᵢ)", labels = "θᵢ=" .* ["Ωr0" "Ωm0" "Ωb0" "H0" "As" "Yp"]); display(p)
