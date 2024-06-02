7fd93e08eb086484f92dfe65a727aaf7c38949c9
Herman Sletmoen <hermansletmoen@gmail.com>
2024-06-02 12:21:01 +0200

    Split spectrum computation into smaller functions

--- Spectra.jl
+++ Spectra.jl
@@ -3,30 +3,26 @@ using Bessels: besselj!, sphericalbesselj
 using ForwardDiff
 using Base.Threads
 
-# matter power spectrum
-P0(k, As) = @. 2*π^2 / k ^ 3 * As # TODO: add kpivot and ns
+# primordial power spectrum
+P0(k, As) = @. 2*π^2 / k^3 * As # TODO: add kpivot and ns
+
+# total matter power spectrum
 function P(pt::PerturbationsSystem, k, Ωr0, Ωm0, Ωb0, h, As, Yp)
     pt_sols = solve(pt, k, Ωr0, Ωm0, Ωb0, h, Yp)
     ηtoday = pt_sols[1].prob.tspan[end] # TODO: something more robust?
     return P0(k, As) .* pt_sols(ηtoday, idxs=pt.sys.Δm) .^ 2
 end
 
-function D_spline(y, x; Spline = CubicSpline, order = 1)
-    y_spline = Spline(y, x; extrapolate=true)
-    y′ = DataInterpolations.derivative.(Ref(y_spline), x, order)
-    return y′
-end
-
-range_until(start, stop, step; skip_start=false) = range(skip_start ? start+step : start, step=step, length=Int(ceil((stop-start)/step+1)))
-# TODO: only need as from a = 1e-4 till today
-function S(pt::PerturbationsSystem, ηs::AbstractArray, ks::AbstractArray, Ωr0, Ωm0, Ωb0, h, Yp; Sspline_ks=nothing)    
+# source function
+function S(pt::PerturbationsSystem, ηs::AbstractArray, ks::AbstractArray, Ωr0, Ωm0, Ωb0, h, Yp; Sspline_ks=nothing) 
     Ss = zeros(eltype([Ωr0, Ωm0, Ωb0, h, Yp]), (length(ηs), length(ks)))
 
     if !isnothing(Sspline_ks)
         Ssplinedata = S(pt, ηs, Sspline_ks, Ωr0, Ωm0, Ωb0, h, Yp)
-        for i_η in eachindex(ηs)
-            Sspline = CubicSpline(Ssplinedata[i_η,:], Sspline_ks)
-            Ss[i_η,:] .= Sspline(ks)
+        for iη in eachindex(ηs)
+            Ssplinedataη = @view Ssplinedata[iη,:]
+            Sspline = CubicSpline(Ssplinedataη, Sspline_ks)
+            Ss[iη,:] .= Sspline(ks)
         end
         return Ss
     end
@@ -43,7 +39,9 @@ function S(pt::PerturbationsSystem, ηs::AbstractArray, ks::AbstractArray, Ωr0,
     
     # TODO: use saveat for ηs
     pt_sols = solve(pt, ks, Ωr0, Ωm0, Ωb0, h, Yp)
-    for (i_k, (k, pt_sol)) in enumerate(zip(ks, pt_sols)) # TODO: parallellize over threads?
+    @threads for ik in eachindex(ks) # TODO: parallellize over threads?
+        k = ks[ik]
+        pt_sol = pt_sols[ik]
         # TODO: must be faster!! use saveat for ηs in ODESolution?
         # TODO: add source functions as observed perturbation functions? but difficult with cumulative τ(η)? must anyway wait for this to be fixed: https://github.com/SciML/ModelingToolkit.jl/issues/2697
         Θ0 = pt_sol(ηs, idxs=pt.sys.ph.Θ0).u
@@ -54,7 +52,7 @@ function S(pt::PerturbationsSystem, ηs::AbstractArray, ks::AbstractArray, Ωr0,
         Ψ′ = D_spline(Ψ, ηs) # TODO: use pt_sol(..., Val{1}) when this is fixed: https://github.com/SciML/ModelingToolkit.jl/issues/2697 and https://github.com/SciML/ModelingToolkit.jl/pull/2574
         Φ′ = D_spline(Φ, ηs)
         ub′ = D_spline(ub, ηs)
-        @. Ss[:,i_k] = g*(Θ0+Ψ+Π/4) + (g′*ub+g*ub′)/k + exp(-τ)*(Ψ′-Φ′) # SW + Doppler + ISW # TODO: add polarization
+        @. Ss[:,ik] = g*(Θ0+Ψ+Π/4) + (g′*ub+g*ub′)/k + exp(-τ)*(Ψ′-Φ′) # SW + Doppler + ISW # TODO: add polarization
     end
 
     return Ss
@@ -68,37 +66,35 @@ end
 #sphericalbesseljfast(ls::AbstractRange, x) = (x == 0.0 ? 1.0 : √(π/(2*x))) * besselj(ls .+ 0.5, x)
 function sphericalbesseljfast!(out, ls::AbstractRange, x)
     besselj!(out, ls .+ 0.5, x)
-    if x != 0.0
+    if x != 0.0 # TODO: well-behaved?
         @. out *= √(π/(2*x))
     end
     return out
 end
 
-# TODO: integrate CubicSplines instead of trapz! https://discourse.julialang.org/t/how-to-speed-up-the-numerical-integration-with-interpolation/96223/5
-function Cl(pt::PerturbationsSystem, ls::AbstractArray, ks::AbstractRange, lnηs::AbstractRange, Ωr0, Ωm0, Ωb0, h, As, Yp; kwargs...)
+# TODO: allocations?
+# TODO: line-of-sight integrate Θl using ODE for evolution of Jl?
+# TODO: spline sphericalbesselj for each l, from x=0 to x=kmax*(η0-ηini)
+# TODO: integrate with ApproxFun? see e.g. https://discourse.julialang.org/t/evaluate-integral-on-many-points-cubature-jl/1723/2
+# line of sight integration
+function Θl(ls::AbstractArray, ks::AbstractRange, lnηs::AbstractRange, Ss::AbstractArray; integrator = SimpsonEven())
     ηs = exp.(lnηs)
     η0 = ηs[end]
-    Ss = S(pt, ηs, ks, Ωr0, Ωm0, Ωb0, h, Yp; kwargs...) # TODO: reduce memory allocation!
 
-    T = eltype([Ωr0, Ωm0, Ωb0, h, As, Yp]) # TODO: handle with/without As differently?
-    ∂Θ_∂lnη = [zeros(T, (length(ls), length(ηs))) for _ in 1:nthreads()] # TODO: best to do array of arrays without using @view, or to use matrix + @view?
-    Θls = zeros(T, (length(ls), length(ks)))
+    Θls = similar(Ss, (length(ls), length(ks)))
+    ∂Θ_∂lnη = [similar(Ss, (length(ls), length(ηs))) for _ in 1:nthreads()] # TODO: best to do array of arrays without using @view, or to use matrix + @view?
     lmin, lmax = extrema(ls)
     ls_all = lmin:1:lmax # range with step 1
     Jls_all = [zeros(length(ls_all)) for _ in 1:nthreads()] # separate workspace per thread
 
-    # TODO: line-of-sight integrate Θl using ODE for evolution of Jl?
-    # TODO: try to spline sphericalbesselj for each l, from x=0 to x=kmax*(η0-ηini)
-    # TODO: integrate with ApproxFun? see e.g. https://discourse.julialang.org/t/evaluate-integral-on-many-points-cubature-jl/1723/2
-    # TODO: separate into line-of-sight integration function
-    @threads for ik in eachindex(ks) # TODO: parallellize over k, too? must extend ∂Θ_∂lnη to nthreas() x η x l, too much memory?
+    @threads for ik in eachindex(ks)
         k = ks[ik]
         for iη in eachindex(ηs)
             η = ηs[iη]
             Sη = Ss[iη,ik] * η
             kΔη = k * (η0-η)
             sphericalbesseljfast!(Jls_all[threadid()], ls_all, kΔη) # TODO: reuse ∂Θ_∂lnη's memory?
-            for il in eachindex(ls)
+            for il in eachindex(ls) # TODO: @simd if I can make Jl access with unit stride? also need @inbounds?
                 l = ls[il]
                 Jl = Jls_all[threadid()][1+l-lmin]
                 ∂Θ_∂lnη[threadid()][il,iη] = Sη * Jl
@@ -106,17 +102,28 @@ function Cl(pt::PerturbationsSystem, ls::AbstractArray, ks::AbstractRange, lnηs
         end
         for il in eachindex(ls)
             integrand = @view ∂Θ_∂lnη[threadid()][il,:]
-            Θls[il, ik] = integrate(lnηs, integrand, SimpsonEven()) # integrate over η # TODO: add starting Θl(ηini) # TODO: calculate ∂Θ_∂logΘ and use Even() methods
+            Θls[il, ik] = integrate(lnηs, integrand, integrator) # integrate over η # TODO: add starting Θl(ηini) # TODO: calculate ∂Θ_∂logΘ and use Even() methods
         end
     end
 
+    return Θls
+end
+
+function Θl(pt::PerturbationsSystem, ls::AbstractArray, ks::AbstractRange, lnηs::AbstractRange, Ωr0, Ωm0, Ωb0, h, As, Yp; kwargs...)
+    ηs = exp.(lnηs)
+    Ss = S(pt, ηs, ks, Ωr0, Ωm0, Ωb0, h, Yp; kwargs...)
+    return Θl(ls, ks, lnηs, Ss)
+end
+
+# TODO: integrate CubicSplines instead of trapz! https://discourse.julialang.org/t/how-to-speed-up-the-numerical-integration-with-interpolation/96223/5
+function Cl(ls::AbstractArray, ks::AbstractRange, Θls::AbstractArray, P0s::AbstractArray; integrator = SimpsonEven())
+    Cls = similar(Θls, length(ls))
     ks_with0 = [0.0; ks]
-    dCl_dks_with0 = [zeros(T, length(ks_with0)) for _ in 1:nthreads()] # separate workspace per thread
-    Cls = zeros(T, length(ls))
+    dCl_dks_with0 = [zeros(eltype(Θls), length(ks_with0)) for _ in 1:nthreads()] # separate workspace per thread
 
     @threads for il in eachindex(ls)
-        @. dCl_dks_with0[threadid()][2:end] = 2/π * ks^2 * P0(ks, As) * Θls[il, :]^2
-        Cls[il] = integrate(ks_with0, dCl_dks_with0[threadid()], SimpsonEven()) # integrate over k (_with0 adds one additional point at (0,0))
+        @. dCl_dks_with0[threadid()][2:end] = 2/π * ks^2 * P0s * Θls[il,:]^2
+        Cls[il] = integrate(ks_with0, dCl_dks_with0[threadid()], integrator) # integrate over k (_with0 adds one additional point at (0,0))
     end
 
     return Cls
@@ -126,21 +133,31 @@ function Cl(pt::PerturbationsSystem, ls::AbstractArray, Ωr0, Ωm0, Ωb0, h, As,
     bg_sol = solve(pt.bg, Ωr0, Ωm0)
 
     ηi, η0 = max(bg_sol[η][begin], 1e-4), bg_sol[η][end]
-    ηi, η0 = ForwardDiff.value.([ηi, η0]) # TODO: do I lose some gradient information here?!
+    ηi, η0 = ForwardDiff.value.([ηi, η0]) # TODO: do I lose some gradient information here?! no? ηi/η0 is just a shift of the integration interval?
     lnηs = range(log(ηi), log(η0), step=Δlnη) # logarithmic spread to capture early-time oscillations # TODO: dynamic/adaptive spacing!
 
     kmax = 2.0 * ls[end]
     ks = range_until(0, kmax, Δk; skip_start=true) ./ η0
-    if splineS
-        Sspline_ks = range_until(0, kmax, 50; skip_start=true) ./ η0 # Δk = 50/η0
-    else
-        Sspline_ks = nothing
-    end
+    Sspline_ks = splineS ? range_until(0, kmax, 50; skip_start=true) ./ η0 : nothing # Δk = 50/η0
 
     return Cl(pt, ls, ks, lnηs, Ωr0, Ωm0, Ωb0, h, As, Yp; Sspline_ks)
 end
 
-Dl(pt::PerturbationsSystem, ls::AbstractArray, args...; kwargs...) = Cl(pt, ls, args...; kwargs...) .* ls .* (ls .+ 1) / (2*π)
+function Cl(pt::PerturbationsSystem, ls::AbstractArray, ks::AbstractRange, lnηs::AbstractRange, Ωr0, Ωm0, Ωb0, h, As, Yp; kwargs...)
+    Θls = Θl(pt, ls, ks, lnηs, Ωr0, Ωm0, Ωb0, h, As, Yp; kwargs...)
+    P0s = P0(ks, As)
+    return Cl(ls, ks, Θls, P0s)
+end
+
+Dl(pt::PerturbationsSystem, ls::AbstractArray, args...; kwargs...) = Cl(pt, ls, args...; kwargs...) .* ls .* (ls .+ 1) ./ 2π
 
 #Dls = Dl(ls, ks, ηs, Ωr0, Ωm0, Ωb0, h, As, Yp; Sspline_ks)
 #plot(ls, Dls; xlabel="l", ylabel="Dl = l (l+1) Cl / 2π")
+
+function D_spline(y, x; Spline = CubicSpline, order = 1)
+    y_spline = Spline(y, x; extrapolate=true)
+    y′ = DataInterpolations.derivative.(Ref(y_spline), x, order)
+    return y′
+end
+
+range_until(start, stop, step; skip_start=false) = range(skip_start ? start+step : start, step=step, length=Int(ceil((stop-start)/step+1)))
