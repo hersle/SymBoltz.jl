3e2ffe9fa96c13952f88bf22e04fc1ec71afd6ed
Herman Sletmoen <hermansletmoen@gmail.com>
2024-06-05 18:48:01 +0200

    Update TODOs

--- Symboltz.jl
+++ Symboltz.jl
@@ -7,10 +7,8 @@ import DifferentialEquations: solve
 using DataInterpolations
 
 # TODO: shooting method https://docs.sciml.ai/DiffEqDocs/stable/tutorials/bvp_example/ (not supported by ModelingToolkit: https://github.com/SciML/ModelingToolkit.jl/issues/924, https://discourse.julialang.org/t/boundary-value-problem-with-modellingtoolkit-or-diffeqoperators/57656)
-# TODO: @register_symbolic from bg -> thermo -> pert instead of reintegrating: https://docs.sciml.ai/ModelingToolkit/stable/tutorials/ode_modeling/#Specifying-a-time-variable-forcing-function
 # TODO: make simpler Cosmology interface
 # TODO: compare runtime for finite vs. dlgP_dlgks_autodiff
-# TODO: compare accuracy with class
 # TODO: non-linear: higher-order perturbations vs halofit vs N-body?
 # TODO: baryons: Recfast -> Recfast++ -> CosmoRec -> HyRec -> HyRec-2: call out, or integrate equations into my code to make use of my background calculation?
 # TODO: composable models, generate equations
@@ -18,13 +16,13 @@ using DataInterpolations
 # TODO: GPU-parallellized EnsembleProblem
 # TODO: relate parameters through parameter expressions: https://docs.sciml.ai/ModelingToolkit/stable/basics/Composition/#Variable-scope-and-parameter-expressions
 # TODO: define components with @mtkmodel?
+# TODO: try different AD sensitivity algorithms: https://docs.sciml.ai/SciMLSensitivity/stable/getting_started/
+# TODO: define global spacetime structure here?
 
 include("Constants.jl")
 
 @variables η # in units of 1/H0
 Dη = Differential(η) # d/dη
-
-# TODO: define global spacetime structure here?
 
 include("Background.jl")
 include("Thermodynamics.jl")
