07eca86e70465c78b685b638a86a9e66cec253ef
Herman Sletmoen <hermansletmoen@gmail.com>
2024-06-09 19:37:55 +0200

    Fix neutrino IC

--- Perturbations.jl
+++ Perturbations.jl
@@ -63,9 +63,11 @@ function perturbations_photon_hierarchy(g0, g1, lmax=6, polarization=true; kwarg
     return ODESystem(eqs, η; defaults, kwargs...)
 end
 
-function perturbations_massless_neutrino_hierarchy(g0, g1, neu0, lmax=6; kwargs...)
+function perturbations_massless_neutrino_hierarchy(g0, g1, neu0, ph0, lmax=6; kwargs...)
     @variables Θ0(η) Θ(η)[1:lmax] δ(η)
-    ρ0 = neu0.ρ0 |> ParentScope # TODO: remove this hack to set IC Θ[2] from background neutrinos; just set Θ[l≥2] = 0 since they are so small anyway?
+    ρν0 = neu0.ρ0 |> ParentScope # TODO: remove this hack to set IC Θ[2] from background neutrinos; just set Θ[l≥2] = 0 since they are so small anyway?
+    ργ0 = ph0.ρ0 |> ParentScope # TODO: remove this hack to set IC Θ[2] from background neutrinos; just set Θ[l≥2] = 0 since they are so small anyway?
+    ρr0 = ργ0 + ρν0
     eqs = [
         Dη(Θ0) ~ -g1.k*Θ[1] - Dη(g1.Φ)
         Dη(Θ[1]) ~ g1.k/3 * (Θ0 - 2*Θ[2] + g1.Ψ)
@@ -76,7 +78,7 @@ function perturbations_massless_neutrino_hierarchy(g0, g1, neu0, lmax=6; kwargs.
     defaults = [
         Θ0 => 1/2 * g1.Φ,
         Θ[1] => -1/6 * g1.k/g0.ℰ * g1.Φ,
-        Θ[2] => -g1.k^2*g0.a^2*(g1.Φ+g1.Ψ) / (32π*ρ0), # TODO: how to set ICs consistently with Ψ, Π and Θν2?
+        Θ[2] => -g1.k^2*g0.a^2 / (32π * (15/4*ρr0 + ρν0)), # TODO: how to set ICs consistently with Ψ, Π and Θν2?
         [Θ[l] => g1.k/((2*l+1)*g0.ℰ) * Θ[l-1] for l in 3:lmax]...
     ]
     return ODESystem(eqs, η; defaults, kwargs...)
@@ -94,11 +96,11 @@ function perturbations_ΛCDM(th::ThermodynamicsSystem, lmax::Int; kwargs...)
     bg = th.bg
     @named g1 = Symboltz.perturbations_metric()
     @named ph = Symboltz.perturbations_photon_hierarchy(bg.sys.g, g1, lmax, true)
-    @named neu = Symboltz.perturbations_massless_neutrino_hierarchy(bg.sys.g, g1, bg.sys.neu, lmax)
+    @named neu = Symboltz.perturbations_massless_neutrino_hierarchy(bg.sys.g, g1, bg.sys.neu, bg.sys.ph, lmax)
     @named cdm = Symboltz.perturbations_matter(bg.sys.g, g1; uinteract=false)
     @named bar = Symboltz.perturbations_matter(bg.sys.g, g1; uinteract=true)
     @named gravpt = Symboltz.perturbations_gravity(bg.sys.g, g1)
-    fν = bg.sys.ph.Ω0 / (bg.sys.ph.Ω0 + bg.sys.neu.Ω0)
+    fν = bg.sys.neu.Ω0 / (bg.sys.ph.Ω0 + bg.sys.neu.Ω0)
     defaults = [
         g1.Ψ => -1/(3/2 + 2*fν/5),
         g1.Φ => -(1 + 2*fν/5) * g1.Ψ
