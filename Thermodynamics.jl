5444ffa86cfb1d75fe23afd26752be6ddca7ed04
Herman Sletmoen <hermansletmoen@gmail.com>
2024-06-18 13:14:06 +0200

    Neatly sum corrections to 1/KHe

--- Thermodynamics.jl
+++ Thermodynamics.jl
@@ -109,7 +109,7 @@ function ThermodynamicsSystem(bg::BackgroundSystem, atoms::AbstractArray{ODESyst
     @parameters Tγ0 Yp fHe = Yp / (mHe_mH*(1-Yp)) # fHe = nHe/nH
     @variables Xe(η) ne(η) τ(η) = 0.0 dτ(η) ρb(η) Tγ(η) Tb(η) βb(η) cs²(η) λe(η)
     @variables XH⁺(η) nH(η) αH(η) βH(η) KH(η) CH(η) # H <-> H⁺
-    @variables XHe⁺(η) nHe(η) αHe(η) βHe(η) KHe(η) KHe0(η) KHe1(η) KHe2(η) γ2Ps(η) CHe(η) # He <-> He⁺
+    @variables XHe⁺(η) nHe(η) αHe(η) βHe(η) KHe(η) KHe0⁻¹(η) KHe1⁻¹(η) KHe2⁻¹(η) γ2Ps(η) CHe(η) # He <-> He⁺
     @variables XHe⁺⁺(η) RHe⁺(η) AHe⁺(η) BHe⁺(η) CHe⁺(η) # He⁺ <-> He⁺⁺
     push!(defaults, Tγ0 => (bg.sys.ph.ρ0 * 15/π^2 * bg.sys.g.H0^2/G * ħ^3*c^5)^(1/4) / kB) # TODO: make part of background species?
 
@@ -157,11 +157,11 @@ function ThermodynamicsSystem(bg::BackgroundSystem, atoms::AbstractArray{ODESyst
         # Helium I recombo (RECFAST: https://arxiv.org/pdf/astro-ph/9909275 + https://arxiv.org/abs/astro-ph/9912182)
         αHe ~ 10^(-16.744) / (√(Tb/3.0) * (1+√(Tb/3.0))^(1-0.711) * (1+√(Tb/10^5.114))^(1+0.711)) # fitting formula
         βHe ~ 4 * αHe / λe^3 * exp(-βb*E_He_∞_2s)
-        KHe0 ~ λ_He_2p_1s^3 / (8π*g.H) # RECFAST He flag 0
-        KHe1 ~ KHe0 / ((1 - exp(-3*A2Ps*nHe*(1-XHe⁺)*KHe0)) + 1e-10) # RECFAST He flag 1 (close to zero modification?) # TODO: not that good, reliability depends on ϵ to avoid division by 0; try to use proper Saha ICs with XHe⁺ ≠ 1.0 and remove it
+        KHe0⁻¹ ~ (8π*g.H) / λ_He_2p_1s^3 # RECFAST He flag 0
+        KHe1⁻¹ ~ -exp(-3*A2Ps*nHe*(1-XHe⁺+1e-10)/KHe0⁻¹) * KHe0⁻¹ # RECFAST He flag 1 (close to zero modification?) # TODO: not that good, reliability depends on ϵ to avoid division by 0; try to use proper Saha ICs with XHe⁺ ≠ 1.0 and remove it
         γ2Ps ~ 3*A2Ps*fHe*(1-XHe⁺+1e-10)*c^2 / (1.436289e-22*8π*√(2π/(βb*mp*mHe_mH*c^2))*(1-XH⁺+1e-10)*(c/λ_He_2p_1s)^3) # TODO: introduce mHe and ν_He_2p_1s?
-        KHe2 ~ 1 / (A2Ps/(1+0.36*γ2Ps^0.86)*3*nHe*(1-XHe⁺) + 1e-10) # RECFAST He flag 2 (Doppler correction) # TODO: increase reliability, particularly at initial time
-        KHe ~ 1 / (1/KHe1 + 1/KHe2) # TODO: 1/KHe additive?
+        KHe2⁻¹ ~ A2Ps/(1+0.36*γ2Ps^0.86)*3*nHe*(1-XHe⁺) # RECFAST He flag 2 (Doppler correction) # TODO: increase reliability, particularly at initial time
+        KHe ~ 1 / (KHe0⁻¹ + KHe1⁻¹ + KHe2⁻¹) # corrections to inverse KHe are additive
         CHe ~ (1 + KHe*ΛHe*nHe*(1-XHe⁺)*exp(-βb*E_He_2p_2s)) / (1 + KHe*(ΛHe+βHe)*nHe*(1-XHe⁺)*exp(-βb*E_He_2p_2s))
         Dη(XHe⁺) ~ -g.a/g.H0 * CHe * (XHe⁺*ne*αHe - βHe*(1-XHe⁺)*exp(-βb*E_He_2s_1s))
 
