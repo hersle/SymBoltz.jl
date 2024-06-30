3be5c55cbfe473e0903072996c219b1ba01de93a
Herman Sletmoen <hermansletmoen@gmail.com>
2024-06-30 18:18:56 +0200

    Use weighted integral for background massive neutrinos, too

--- src/Background.jl
+++ src/Background.jl
@@ -37,21 +37,26 @@ function background_photons(g; kwargs...)
     ], t; name=:ph))
 end
 
-Iρmν(y) = ∫(x -> x^2 * √(x^2+y^2) / (exp(x) + 1), 0, Inf) # Iρ(0) = 7π^4 / 120 # TODO: exp with x or √(x^2+y^2)?
-IPmν(y) = ∫(x -> x^4 / √(x^2+y^2) / (exp(x) + 1), 0, Inf) # IP(0) = Iρ(0) # TODO: exp with x or √(x^2+y^2)?
-@register_symbolic Iρmν(y)
-@register_symbolic IPmν(y)
-function background_massive_neutrinos(g; kwargs...)
-    pars = @parameters Ω0 ρ0 m ∑m T0 y0
+function background_massive_neutrinos(g; nx=20, xmax=50.0, kwargs...)
+    pars = @parameters Ω0_massless Ω0 ρ0 m ∑m T0 y0
     vars = @variables ρ(t) T(t) y(t) P(t) w(t)
+
+    # TODO: also allow adaptive quadgk
+    x, W = gauss(nx, 0.0, xmax)
+    f0(x) = 1 / (exp(x) + 1) # TODO: exp(x) or exp(ϵ)
+    ϵ(x, y) = √(x^2 + y^2)
+    Iρ(y) = ∫((@. x^2 * ϵ(x, y) * f0(x)), W) # Iρ(0) = 7π^4/120
+    IP(y) = ∫((@. x^4 / ϵ(x, y) * f0(x)), W) # IP(0) = Iρ(0)
+
     eqs = [
         T ~ T0 / g.a
         y ~ m*c^2 / (kB*T)
-        ρ ~ ρ0/g.a^4 * Iρmν(y) / Iρmν(y0) # have ρ = Cρ * Iρ(y) / a⁴, so Cρ = ρ0 * 1⁴ / Iρ(y0)
-        P ~ 1/3 * ρ0/g.a^4 * IPmν(y) / Iρmν(y0) # have P = CP * IP(y) / a⁴, and in the early universe Iρ(y→0) → IP(y→0) and P/ρ = CP * IP(y) / (Cρ * Iρ(y)) → CP/Cρ → 1/3, so CP = Cρ/3
+        ρ ~ ρ0/g.a^4 * Iρ(y) / Iρ(y0) # have ρ = Cρ * Iρ(y) / a⁴, so Cρ = ρ0 * 1⁴ / Iρ(y0)
+        P ~ 1/3 * ρ0/g.a^4 * IP(y) / Iρ(y0) # have P = CP * IP(y) / a⁴, and in the early universe Iρ(y→0) → IP(y→0) and P/ρ = CP * IP(y) / (Cρ * Iρ(y)) → CP/Cρ → 1/3, so CP = Cρ/3
         w ~ P / ρ
     ]
     defaults = [
+        Ω0 => Ω0_massless * Iρ(y0)/Iρ(0) * y0 # ≈ Ω0_massless * (3ζ(3)/2)/(7π^4/120)*y0 for y0 → ∞
         ρ0 => 3/8π * Ω0
         ∑m => 0.06 * eV/c^2 # total mass
         m => ∑m / 3 # single mass
@@ -70,13 +75,13 @@ function background_ΛCDM(; kwargs...)
     @named bar = background_matter(g)
     @named de = background_cosmological_constant(g)
     species = [ph, neu, mneu, cdm, bar, de]
-    initialization_eqs = [g.a ~ (2t)^(1/2) * (ph.Ω0 + neu.Ω0 + mneu.Ω0 * Iρmν(0)/Iρmν(mneu.y0))^(1/4)] # analytical radiation-dominated solution # TODO: add effect from massive neutrinos
+    initialization_eqs = [g.a ~ (2t)^(1/2) * (ph.Ω0 + neu.Ω0 + mneu.Ω0_massless)^(1/4)] # analytical radiation-dominated solution # TODO: add effect from massive neutrinos
     defaults = [
         species[end].Ω0 => 1 - sum(s.Ω0 for s in species[begin:end-1]) # TODO: solve nonlinear system
         ph.T0 => (ph.ρ0 * 15/π^2 * g.H0^2/G * ħ^3*c^5)^(1/4) / kB # TODO: move to photon system
         neu.Ω0 => (3.046/3) * (4/11)^(4/3) * ph.Ω0 # TODO: make Neff parameter
         mneu.T0 => (3.046/3)^(1/4) * (4/11)^(1/3) * ph.T0 # same as for massless neutrinos # TODO: make Neff parameter etc.
-        mneu.Ω0 => neu.Ω0 * Iρmν(mneu.y0)/Iρmν(0) # (3ζ(3)/2) / (7π^4/120) * y0 ≈ Iρmν(y0) / Iρmν(0)
+        mneu.Ω0_massless => neu.Ω0 # Ω0 for corresponding massless neutrinos
     ]
     eqs = [grav.ρ ~ sum(s.ρ for s in species)]
     comps = [g; grav; species]
