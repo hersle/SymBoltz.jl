c213c283888670bf0dee4ff3a54a806f167dbc80
Herman Sletmoen <hermansletmoen@gmail.com>
2024-07-12 15:04:21 +0200

    Simplify order specification with O(ϵ^n)

--- src/Components.jl
+++ src/Components.jl
@@ -13,35 +13,40 @@ end
 
 function gravity(g; kwargs...)
     @variables ρ(t) ρcrit(t) δρ(t) Π(t)
-    return ODESystem([
+    eqs0 = [
         D(g.a) ~ √(8π/3 * ρ) * g.a^2 # Friedmann equation
         ρcrit ~ 3/8π * g.E^2 # critical density (H² = 8πG/3 * ρcrit)
-        ϵ * D(g.Φ) ~ (-4π/3*g.a^2/g.ℰ*δρ - k^2/(3*g.ℰ)*g.Φ - g.ℰ*g.Ψ) * ϵ
-        ϵ * k^2 * (g.Φ - g.Ψ) ~ 12π * g.a^2 * Π * ϵ
-    ], t, [ρ, ρcrit, δρ, Π], []; kwargs...)
+    ] .|> O(ϵ^0)
+    eqs1 = [
+        D(g.Φ) ~ -4π/3*g.a^2/g.ℰ*δρ - k^2/(3*g.ℰ)*g.Φ - g.ℰ*g.Ψ
+        k^2 * (g.Φ - g.Ψ) ~ 12π * g.a^2 * Π
+    ] .|> O(ϵ^1)
+    return ODESystem([eqs0; eqs1], t, [ρ, ρcrit, δρ, Π], []; kwargs...)
 end
 
 function species_constant_eos(g, w, cs² = w, ẇ = 0, _σ = 0; θinteract = false, kwargs...)
     @assert ẇ == 0 && _σ == 0 # TODO: relax (need to include in ICs)
-    pars = @parameters Ω0 ρ0
-    vars = @variables ρ(t) P(t) δ(t) θ(t) Δ(t) θinteraction(t) σ(t)
-    eqs = [
+    pars = @parameters Ω0 ρ0 # TODO: split pars0, pars1
+    vars = @variables ρ(t) P(t) δ(t) θ(t) Δ(t) θinteraction(t) σ(t) # TODO: split vars0, vars1
+    eqs0 = [
         P ~ w * ρ # equation of state
         ρ ~ ρ0 * g.a^(-3*(1+w)) # alternative derivative: D(ρ) ~ -3 * g.ℰ * (ρ + P)
-        ϵ*D(δ) ~ (-(1+w)*(θ-3*D(g.Φ)) - 3*g.ℰ*(cs²-w)*δ) * ϵ # Bertschinger & Ma (30) with Φ -> -Φ; or Baumann (4.4.173) with Φ -> -Φ
-        ϵ*D(θ) ~ (-g.ℰ*(1-3*w)*θ - ẇ/(1+w)*θ + cs²/(1+w)*k^2*δ - k^2*σ + k^2*g.Ψ + θinteraction) * ϵ # Bertschinger & Ma (30) with θ = kv
-        ϵ*Δ ~ (δ + 3(1+w) * g.ℰ/θ) * ϵ # Baumann (4.2.144) with v -> -u
-        ϵ*σ ~ ϵ*_σ
-    ]
-    initialization_eqs = [
-        ϵ*δ ~ ϵ * -3/2 * (1+w) * g.Ψ # adiabatic: δᵢ/(1+wᵢ) == δⱼ/(1+wⱼ) (https://cmb.wintherscoming.no/theory_initial.php#adiabatic)
-        ϵ*θ ~ ϵ * 1/2 * (k^2*t) * g.Ψ # # TODO: fix with θ: -1/2 * k*t * g.Ψ # TODO: include σ ≠ 0 # solve u′ + ℋ(1-3w)u = w/(1+w)*kδ + kΨ with Ψ=const, IC for δ, Φ=-Ψ, ℋ=H₀√(Ωᵣ₀)/a after converting ′ -> d/da by gathering terms with u′ and u in one derivative using the trick to multiply by exp(X(a)) such that X′(a) will "match" the terms in front of u
-    ]
-    defaults = [
+    ] .|> O(ϵ^0)
+    eqs1 = [
+        D(δ) ~ -(1+w)*(θ-3*D(g.Φ)) - 3*g.ℰ*(cs²-w)*δ # Bertschinger & Ma (30) with Φ -> -Φ; or Baumann (4.4.173) with Φ -> -Φ
+        D(θ) ~ -g.ℰ*(1-3*w)*θ - ẇ/(1+w)*θ + cs²/(1+w)*k^2*δ - k^2*σ + k^2*g.Ψ + θinteraction # Bertschinger & Ma (30) with θ = kv
+        Δ ~ δ + 3(1+w) * g.ℰ/θ # Baumann (4.2.144) with v -> -u
+        σ ~ _σ
+    ] .|> O(ϵ^1)
+    ics1 = [
+        δ ~ -3/2 * (1+w) * g.Ψ # adiabatic: δᵢ/(1+wᵢ) == δⱼ/(1+wⱼ) (https://cmb.wintherscoming.no/theory_initial.php#adiabatic)
+        θ ~ 1/2 * (k^2*t) * g.Ψ # # TODO: fix with θ: -1/2 * k*t * g.Ψ # TODO: include σ ≠ 0 # solve u′ + ℋ(1-3w)u = w/(1+w)*kδ + kΨ with Ψ=const, IC for δ, Φ=-Ψ, ℋ=H₀√(Ωᵣ₀)/a after converting ′ -> d/da by gathering terms with u′ and u in one derivative using the trick to multiply by exp(X(a)) such that X′(a) will "match" the terms in front of u
+    ] .|> O(ϵ^1)
+    defs = [
         ρ0 => 3/8π*Ω0
     ]
-    !θinteract && push!(eqs, ϵ*θinteraction ~ 0)
-    return ODESystem(eqs, t, vars, pars; initialization_eqs, defaults, kwargs...)
+    !θinteract && push!(eqs1, (θinteraction ~ 0) |> O(ϵ^1))
+    return ODESystem([eqs0; eqs1], t, vars, pars; initialization_eqs=ics1, defaults=defs, kwargs...)
 end
 
 function matter(g; kwargs...)
@@ -57,8 +62,8 @@ function cosmological_constant(g; kwargs...)
     Λ = background(Λ) # discard nonexisting perturbations
     @variables δ(t) σ(t)
     @parameters ρ0 Ω0 # TODO: avoid re-creating
-    defaults = [ρ0 => 3/8π*Ω0]
-    Λ = extend(ODESystem([ϵ*δ ~ 0, ϵ*σ ~ 0], t, [δ, σ], [ρ0, Ω0]; defaults, name=:Λ), complete(Λ)) # no perturbations
+    defs = [ρ0 => 3/8π*Ω0]
+    Λ = extend(ODESystem([δ ~ 0, σ ~ 0] .|> O(ϵ^1), t, [δ, σ], [ρ0, Ω0]; defaults=defs, name=:Λ), complete(Λ)) # no perturbations
     return Λ
 end
 
@@ -75,36 +80,36 @@ function photons(g; polarization=true, lmax=6, kwargs...)
     ], name=:γ))
 
     # perturbations
-    eqs = [
-        ϵ*D(F0) ~ (-k*F[1] + 4*D(g.Φ)) * ϵ
-        ϵ*D(F[1]) ~ (k/3*(F0-2*F[2]+4*g.Ψ) - 4/3 * τ̇/k * (θb - θ)) * ϵ
-        ϵ*D(F[2]) ~ (2/5*k*F[1] - 3/5*k*F[3] + 9/10*τ̇*F[2] - 1/10*τ̇*(G0+G[2])) * ϵ
-        [ϵ*D(F[l]) ~ (k/(2*l+1) * (l*F[l-1] - (l+1)*F[l+1]) + τ̇*F[l]) * ϵ for l in 3:lmax-1]... # TODO: Π in last term here?
-        ϵ*D(F[lmax]) ~ (k*F[lmax-1] - (lmax+1) / t * F[lmax] + τ̇ * F[lmax]) * ϵ # TODO: assumes lmax ≥ ???
-        ϵ*δ ~ F0 * ϵ
-        ϵ*θ ~ 3/4*k*F[1] * ϵ
-        ϵ*σ ~ F[2]/2 * ϵ
-        ϵ*Π ~ (F[2] + G0 + G[2]) * ϵ
+    eqs1 = [
+        D(F0) ~ -k*F[1] + 4*D(g.Φ)
+        D(F[1]) ~ k/3*(F0-2*F[2]+4*g.Ψ) - 4/3 * τ̇/k * (θb - θ)
+        D(F[2]) ~ 2/5*k*F[1] - 3/5*k*F[3] + 9/10*τ̇*F[2] - 1/10*τ̇*(G0+G[2])
+        [D(F[l]) ~ k/(2*l+1) * (l*F[l-1] - (l+1)*F[l+1]) + τ̇*F[l] for l in 3:lmax-1]... # TODO: Π in last term here?
+        D(F[lmax]) ~ k*F[lmax-1] - (lmax+1) / t * F[lmax] + τ̇ * F[lmax] # TODO: assumes lmax ≥ ???
+        δ ~ F0
+        θ ~ 3/4*k*F[1]
+        σ ~ F[2]/2
+        Π ~ F[2] + G0 + G[2]
         (polarization ? [
-            ϵ*D(G0) ~ (k * (-G[1]) - τ̇ * (-G0 + Π/2)) * ϵ
-            ϵ*D(G[1]) ~ (k/3 * (G0 - 2*G[2]) - τ̇ * (-G[1])) * ϵ
-            [ϵ*D(G[l]) ~ (k/(2*l+1) * (l*G[l-1] - (l+1)*G[l+1]) - τ̇ * (-G[l] + Π/10*δkron(l,2))) * ϵ for l in 2:lmax-1]... # TODO: collect all equations here once G[0] works
-            ϵ*D(G[lmax]) ~ (k*G[lmax-1] - (lmax+1) / t * G[lmax] + τ̇ * G[lmax]) * ϵ
+            D(G0) ~ k * (-G[1]) - τ̇ * (-G0 + Π/2)
+            D(G[1]) ~ k/3 * (G0 - 2*G[2]) - τ̇ * (-G[1])
+            [D(G[l]) ~ k/(2*l+1) * (l*G[l-1] - (l+1)*G[l+1]) - τ̇ * (-G[l] + Π/10*δkron(l,2)) for l in 2:lmax-1]... # TODO: collect all equations here once G[0] works
+            D(G[lmax]) ~ k*G[lmax-1] - (lmax+1) / t * G[lmax] + τ̇ * G[lmax]
         ] : [
-            ϵ*G0 ~ 0, collect(ϵ*G .~ 0)... # pin to zero
+            G0 ~ 0, collect(G .~ 0)... # pin to zero
         ])...
-    ]
-    initialization_eqs = [
-        ϵ*δ ~ -2 * g.Ψ * ϵ # Dodelson (7.89)
-        ϵ*θ ~ 1/2 * (k^2*t) * g.Ψ * ϵ # Dodelson (7.95)
-        ϵ*F[2] ~ 0 # (polarization ? -8/15 : -20/45) * k/dτ * Θ[1], # depends on whether polarization is included # TODO: move to initialization_eqs?
-        [ϵ*F[l] ~ 0 #=-l/(2*l+1) * k/dτ * Θ[l-1]=# for l in 3:lmax]...
-        ϵ*G0 ~ 0 #5/4 * Θ[2],
-        ϵ*G[1] ~ 0 #-1/4 * k/dτ * Θ[2],
-        ϵ*G[2] ~ 0 #1/4 * Θ[2],
-        [ϵ*G[l] ~ 0 #=-l/(2*l+1) * k/dτ * ΘP[l-1]=# for l in 3:lmax]...
-    ]
-    γ = extend(γ, ODESystem(eqs, t, [γ.ρ, δ, θ, σ, τ̇, θb], [T0]; initialization_eqs, kwargs...))
+    ] .|> O(ϵ^1)
+    ics1 = [
+        δ ~ -2 * g.Ψ # Dodelson (7.89)
+        θ ~ 1/2 * (k^2*t) * g.Ψ # Dodelson (7.95)
+        F[2] ~ 0 # (polarization ? -8/15 : -20/45) * k/dτ * Θ[1], # depends on whether polarization is included # TODO: move to initialization_eqs?
+        [F[l] ~ 0 #=-l/(2*l+1) * k/dτ * Θ[l-1]=# for l in 3:lmax]...
+        G0 ~ 0 #5/4 * Θ[2],
+        G[1] ~ 0 #-1/4 * k/dτ * Θ[2],
+        G[2] ~ 0 #1/4 * Θ[2],
+        [G[l] ~ 0 #=-l/(2*l+1) * k/dτ * ΘP[l-1]=# for l in 3:lmax]...
+    ] .|> O(ϵ^1)
+    γ = extend(γ, ODESystem(eqs1, t, [γ.ρ, δ, θ, σ, τ̇, θb], [T0]; initialization_eqs=ics1, kwargs...))
     return γ
 end
 
@@ -113,22 +118,23 @@ function massless_neutrinos(g; lmax=6, kwargs...)
 
     vars = @variables F0(t) F(t)[1:lmax+1] δ(t) θ(t) σ(t)
     pars = @parameters Neff
-    eqs = [
-        ϵ*D(F0) ~ (-k*F[1] + 4*D(g.Φ)) * ϵ
-        ϵ*D(F[1]) ~ (k/3*(F0-2*F[2]+4*g.Ψ)) * ϵ
-        [ϵ*D(F[l]) ~ (k/(2*l+1) * (l*F[l-1] - (l+1)*F[l+1])) * ϵ for l in 2:lmax]...
-        ϵ*F[lmax+1] ~ ((2*lmax+1) / (k*t) * F[lmax] - F[lmax-1]) * ϵ
-        ϵ*δ ~ F0 * ϵ
-        ϵ*θ ~ 3/4*k*F[1] * ϵ
-        ϵ*σ ~ F[2]/2 * ϵ
-    ]
-    initialization_eqs = [
-        ϵ*δ ~ -2 * g.Ψ * ϵ # adiabatic: δᵢ/(1+wᵢ) == δⱼ/(1+wⱼ) (https://cmb.wintherscoming.no/theory_initial.php#adiabatic)
-        ϵ*θ ~ 1/2 * (k^2*t) * g.Ψ * ϵ
-        ϵ*σ ~ 1/15 * (k*t)^2 * g.Ψ * ϵ # TODO: how to set ICs consistently with Ψ, Π and Θν2?
-        [ϵ*F[l] ~ 0 #=1/(2*l+1) * k*t * Θ[l-1]=# for l in 3:lmax]...
-    ]
-    return extend(ν, ODESystem(eqs, t, vars, pars; initialization_eqs, defaults = [Neff => 3.046], kwargs...))
+    eqs1 = [
+        D(F0) ~ -k*F[1] + 4*D(g.Φ)
+        D(F[1]) ~ k/3*(F0-2*F[2]+4*g.Ψ)
+        [D(F[l]) ~ k/(2*l+1) * (l*F[l-1] - (l+1)*F[l+1]) for l in 2:lmax]...
+        F[lmax+1] ~ (2*lmax+1) / (k*t) * F[lmax] - F[lmax-1]
+        δ ~ F0
+        θ ~ 3/4*k*F[1]
+        σ ~ F[2]/2
+    ] .|> O(ϵ^1)
+    ics1 = [
+        δ ~ -2 * g.Ψ # adiabatic: δᵢ/(1+wᵢ) == δⱼ/(1+wⱼ) (https://cmb.wintherscoming.no/theory_initial.php#adiabatic)
+        θ ~ 1/2 * (k^2*t) * g.Ψ
+        σ ~ 1/15 * (k*t)^2 * g.Ψ # TODO: how to set ICs consistently with Ψ, Π and Θν2?
+        [F[l] ~ 0 #=1/(2*l+1) * k*t * Θ[l-1]=# for l in 3:lmax]...
+    ] .|> O(ϵ^1)
+    defs = [Neff => 3.046]
+    return extend(ν, ODESystem(eqs1, t, vars, pars; initialization_eqs=ics1, defaults=defs, kwargs...))
 end
 
 # TODO: use vector equations and simplify loops
@@ -144,39 +150,40 @@ function massive_neutrinos(g; nx=5, lmax=4, kwargs...)
     Iρ(y) = ∫dx_x²_f0(@. E(x, y)) # Iρ(0) = 7π^4/120
     IP(y) = ∫dx_x²_f0(@. x^2 / E(x, y)) # IP(0) = Iρ(0)
     
-    eqs = [
+    eqs0 = [
         T ~ T0 / g.a # TODO: move into radiation?
         y ~ m*c^2 / (kB*T)
         ρ ~ ρ0_massless/g.a^4 * Iρ(y) / Iρ(0) # have ρ = Cρ * Iρ(y) / a⁴, so Cρ = ρ0 * 1⁴ / Iρ(y0) # TODO: div by Iρ(0) or Iρ(y0)?
         P ~ 1/3 * ρ0_massless/g.a^4 * IP(y) / Iρ(0) # have P = CP * IP(y) / a⁴, and in the early universe Iρ(y→0) → IP(y→0) and P/ρ = CP * IP(y) / (Cρ * Iρ(y)) → CP/Cρ → 1/3, so CP = Cρ/3 # TODO: div by Iρ(0) or Iρ(y0)?
         w ~ P / ρ
-
-        ϵ*δ ~ ∫dx_x²_f0(@. E(x, y)*ψ0) / ∫dx_x²_f0(@. E(x, y)) * ϵ
-        ϵ*σ ~ (2/3) * ∫dx_x²_f0(@. x^2/E(x,y)*ψ[:,2]) / (∫dx_x²_f0(@. E(x,y)) + 1/3*∫dx_x²_f0(@. x^2/E(x,y))) * ϵ
-    ]
-    defaults = [
+    ] .|> O(ϵ^0)
+    eqs1 = [
+        δ ~ ∫dx_x²_f0(@. E(x, y)*ψ0) / ∫dx_x²_f0(@. E(x, y))
+        σ ~ (2/3) * ∫dx_x²_f0(@. x^2/E(x,y)*ψ[:,2]) / (∫dx_x²_f0(@. E(x,y)) + 1/3*∫dx_x²_f0(@. x^2/E(x,y)))
+    ] .|> O(ϵ^1)
+    defs = [
         Ω0 => Ω0_massless * Iρ(y0)/Iρ(0) # ≈ Ω0_massless * (3ζ(3)/2)/(7π^4/120) * y0 for y0 → ∞
         ρ0 => 3/8π * Ω0
         ρ0_massless => 3/8π * Ω0_massless
         m => 0.02 * eV/c^2 # one massive neutrino with this mass
         y0 => m*c^2 / (kB*T0)
     ]
-    initialization_eqs = []
+    ics1 = []
     for i in 1:nx
-        push!(eqs, [
-            ϵ*D(ψ0[i]) ~ (-k * x[i]/E(x[i],y) * ψ[i,1] - D(g.Φ) * dlnf0_dlnx(x[i])) * ϵ
-            ϵ*D(ψ[i,1]) ~ (k/3 * x[i]/E(x[i],y) * (ψ0[i] - 2*ψ[i,2]) - k/3 * E(x[i],y)/x[i] * g.Ψ * dlnf0_dlnx(x[i])) * ϵ
-            [ϵ*D(ψ[i,l]) ~ k/(2*l+1) * x[i]/E(x[i],y) * (l*ψ[i,l-1] - (l+1)*ψ[i,l+1]) * ϵ for l in 2:lmax]...
-            ϵ*ψ[i,lmax+1] ~ ((2*lmax+1) * E(x[i],y)/x[i] * ψ[i,lmax] / (k*t) - ψ[i,lmax-1]) * ϵ
-        ]...)
-        push!(initialization_eqs, [
-            ϵ*ψ0[i] ~ -1/4 * (-2*g.Ψ) * dlnf0_dlnx(x[i]) * ϵ
-            ϵ*ψ[i,1] ~ -1/(3*k) * E(x[i],y)/x[i] * (1/2*(k^2*t)*g.Ψ) * dlnf0_dlnx(x[i]) * ϵ
-            ϵ*ψ[i,2] ~ -1/2 * (1/15*(k*t)^2*g.Ψ) * dlnf0_dlnx(x[i]) * ϵ
-            [ϵ*ψ[i,l] ~ 0 for l in 3:lmax] # TODO: proper ICs    
-        ]...)
+        union!(eqs1, [
+            D(ψ0[i]) ~ -k * x[i]/E(x[i],y) * ψ[i,1] - D(g.Φ) * dlnf0_dlnx(x[i])
+            D(ψ[i,1]) ~ k/3 * x[i]/E(x[i],y) * (ψ0[i] - 2*ψ[i,2]) - k/3 * E(x[i],y)/x[i] * g.Ψ * dlnf0_dlnx(x[i])
+            [D(ψ[i,l]) ~ k/(2*l+1) * x[i]/E(x[i],y) * (l*ψ[i,l-1] - (l+1)*ψ[i,l+1]) for l in 2:lmax]...
+            ψ[i,lmax+1] ~ (2*lmax+1) * E(x[i],y)/x[i] * ψ[i,lmax] / (k*t) - ψ[i,lmax-1]
+        ] .|> O(ϵ^1))
+        union!(ics1, [
+            ψ0[i] ~ -1/4 * (-2*g.Ψ) * dlnf0_dlnx(x[i])
+            ψ[i,1] ~ -1/(3*k) * E(x[i],y)/x[i] * (1/2*(k^2*t)*g.Ψ) * dlnf0_dlnx(x[i])
+            ψ[i,2] ~ -1/2 * (1/15*(k*t)^2*g.Ψ) * dlnf0_dlnx(x[i])
+            [ψ[i,l] ~ 0 for l in 3:lmax] # TODO: proper ICs    
+        ] .|> O(ϵ^1))
     end
-    return ODESystem(eqs, t, vars, pars; initialization_eqs, defaults, kwargs...)
+    return ODESystem([eqs0; eqs1], t, vars, pars; initialization_eqs=ics1, defaults=defs, kwargs...)
 end
 
 function baryons(g; recombination=true, kwargs...)
@@ -218,6 +225,10 @@ function identity(sys)
     guesses = ModelingToolkit.get_guesses(sys)
     return ODESystem(eqs, t, vars, pars; initialization_eqs=ieqs, defaults=defs, guesses=guesses, name=sys.name)
 end
+
+O(x, ϵⁿ) = x * ϵⁿ
+O(eq::Equation, ϵⁿ) = O(eq.lhs, ϵⁿ) ~ O(eq.rhs, ϵⁿ)
+O(ϵⁿ) = x -> O(x, ϵⁿ)
 
 function extract_order(expr, order)
     if order == 0
@@ -289,18 +300,19 @@ function ΛCDM(; kwargs...)
         C => 0.48 # TODO: why does ≈ 0.48 give better agreement with CLASS? # TODO: phi set here? https://github.com/lesgourg/class_public/blob/ae99bcea1cd94994228acdfaec70fa8628ae24c5/source/perturbations.c#L5713
         g.Ψ => 20C / (15 #=+ 4fν=#) # Φ found from solving initialization system # TODO: is this correct when having both massless and massive neutrinos?
     ]
-    connections = ODESystem([
+    eqs0 = [
         G.ρ ~ sum(s.ρ for s in species)
-        ϵ*G.δρ ~ ϵ*sum(s.δ * s.ρ for s in species) # total energy density perturbation
-        ϵ*G.Π ~ ϵ*sum((s.ρ + s.P) * s.σ for s in species)
-
         b.rec.ρb ~ b.ρ * g.H0^2/GN # kg/m³ (convert from H0=1 units to SI units)
         b.rec.Tγ ~ γ.T
-        ϵ*b.θinteraction ~ #=g.k^2*csb²*bar.δ +=# -b.rec.dτ * 4*γ.ρ/(3*b.ρ) * (γ.θ - b.θ) * ϵ # TODO: enable csb² when it seems stable... # TODO: define some common interaction type, e.g. momentum transfer
-
-        ϵ*γ.τ̇ ~ b.rec.dτ * ϵ
-        ϵ*γ.θb ~ b.θ * ϵ
-    ], t, [], [C, k]; initialization_eqs, defaults, kwargs...)
+    ] .|> O(ϵ^0)
+    eqs1 = [
+        G.δρ ~ sum(s.δ * s.ρ for s in species) # total energy density perturbation
+        G.Π ~ sum((s.ρ + s.P) * s.σ for s in species)
+        b.θinteraction ~ #=g.k^2*csb²*bar.δ +=# -b.rec.dτ * 4*γ.ρ/(3*b.ρ) * (γ.θ - b.θ) # TODO: enable csb² when it seems stable... # TODO: define some common interaction type, e.g. momentum transfer
+        γ.τ̇ ~ b.rec.dτ
+        γ.θb ~ b.θ
+    ] .|> O(ϵ^1)
+    connections = ODESystem([eqs0; eqs1], t, [], [C, k]; initialization_eqs, defaults, kwargs...)
     return compose(connections, g, G, species...)
 end
 
