912f68f8984adfe5e9aaedcbf0947d7440ee6d53
Herman Sletmoen <hermansletmoen@gmail.com>
2024-09-23 15:35:14 +0200

    Make Brans-Dicke example

--- src/Components.jl
+++ src/Components.jl
@@ -67,24 +67,6 @@ end
 """
     brans_dicke(g; name = :G, kwargs...)
 
-# Examples
-Shoot for parameters that give `E = G = 1` today:
-```@example 1
-using SymBoltz, ModelingToolkit
-M = BDΛCDM()
-D = Differential(M.t)
-
-pars_fixed = [parameters_Planck18(M); M.G.ω => 100.0; D(M.G.ϕ) => 0.0] # unspecified: M.Λ.Ω0, M.G.ϕ
-pars_guess = [M.G.ϕ => 0.95, M.Λ.Ω0 => 0.7] # initial guesses for shooting method
-pars_shoot = shoot(M, pars_fixed, pars_guess, [M.g.ℰ ~ 1, M.G.G ~ 1]; thermo = false, backwards = false) # exact solutions
-pars = [pars_fixed; pars_shoot] # merge fixed and shooting parameters
-```
-Solve background and plot scalar field and Hubble function:
-```@example 1
-using Plots
-sol = solve(M, pars, thermo = false, backwards = false)
-plot(sol, log10(M.g.a), [M.g.ℰ, M.G.G], ylims=(0.8, 1.2))
-```
 """
 function brans_dicke(g; name = :G, kwargs...)
     @parameters ω
@@ -479,6 +461,11 @@ function GRΛCDM(args...; kwargs...)
     return ΛCDM(args...; kwargs...)
 end
 
+"""
+    BDΛCDM(; name = :BDΛCDM, kwargs...)
+
+Create a ΛCDM model, but with the Brans-Dicke theory of gravity instead of General Relativity.
+"""
 function BDΛCDM(; name = :BDΛCDM, kwargs...)
     M = GRΛCDM(; Λanalytical = true, kwargs...)
     G = brans_dicke(M.g)
