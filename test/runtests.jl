9c5f27f640061ecd1f22106f70cc2098fd01f5d5
Herman Sletmoen <hermansletmoen@gmail.com>
2025-01-20 21:40:16 +0100

    Fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using SymBoltz
 
 M = ΛCDM()
 pars = SymBoltz.parameters_Planck18(M)
+prob = CosmologyProblem(M, pars, Dict(M.Λ.Ω₀ => 0.5), [M.g.ℰ ~ 1])
 
 @testset "Solution accessing" begin
     ks = [1e2, 1e3]
@@ -11,7 +12,7 @@ pars = SymBoltz.parameters_Planck18(M)
     nk, nt, ni = length(ks), length(ts), length(is)
 
     # Size of solution output should match input arguments in order
-    sol = solve(M, pars)
+    sol = solve(prob)
     @test size(sol(ts[1], is[1])) == () # background
     @test size(sol(ts[1], is)) == (ni,)
     @test size(sol(ts, is[1])) == (nt,)
@@ -20,7 +21,7 @@ pars = SymBoltz.parameters_Planck18(M)
     #@test_throws "below minimum solved time" sol(sol[M.t][begin]-1, is)
     #@test_throws "above maximum solved time" sol(sol[M.t][end]+1, is)
 
-    sol = solve(M, pars, ks)
+    sol = solve(prob, ks)
     @test size(sol(ks[1], ts[1], is[1])) == () # perturbations
     @test size(sol(ks[1], ts[1], is)) == (ni,)
     @test size(sol(ks[1], ts, is[1])) == (nt,)
@@ -55,7 +56,7 @@ end
 end
 
 @testset "Timeseries" begin
-    sol = solve(M, pars)
+    sol = solve(prob)
     ts = SymBoltz.timeseries(sol; Nextra=1) # naive implementation could transform endpoints slightly through exp(log(t))
     @test sol(ts, M.g.a) isa AbstractArray # should be in bounds
 
