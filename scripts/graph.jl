de3ac588f1c9666befabb4f5a178cc56bf809631
Herman Sletmoen <hermansletmoen@gmail.com>
2025-02-21 12:11:49 +0100

    Clean up test/ directory; move remains to scripts/

--- scripts/graph.jl
+++ scripts/graph.jl
@@ -1,12 +1,9 @@
-using ModelingToolkit
-using Plots; Plots.default(label=nothing)
-using GraphRecipes
+using SymBoltz, ModelingToolkit, Plots, GraphRecipes
+Plots.default(label=nothing)
 
-@named bg = SymBoltz.background_ΛCDM()
-@named th = SymBoltz.thermodynamics_ΛCDM(bg)
-@named pt = SymBoltz.perturbations_ΛCDM(th, 6)
-
-tstate = ModelingToolkit.get_tearing_state(pt.ssys)
+M = ΛCDM(h = nothing, ν = nothing, K = nothing, lmax = 3)
+Ms = structural_simplify(M)
+tstate = ModelingToolkit.get_tearing_state(Ms)
 imat = ModelingToolkit.BipartiteGraphs.incidence_matrix(tstate.structure.graph)
 function plot_dependency_graph(sys)
     tstate = ModelingToolkit.get_tearing_state(sys) # works on simplified systems # TODO: what to do on unsimplified systems?
@@ -16,15 +13,15 @@ function plot_dependency_graph(sys)
 
     # color each variable based on its subsystem
     names_cleaned = replace.(names, (["D", "(", ")"] .=> "")...) # remove any derivative signs
-    subsystems = ['₊' in name ? name[begin:findlast('₊', name)-1] : "" for name in names_cleaned]
+    subsystems = ['₊' in name ? name[begin:prevind(name, findlast('₊', name))] : "" for name in names_cleaned]
     usubsystems = unique(subsystems)
     groups = [findfirst(usub -> usub == sub, usubsystems) for sub in subsystems]
 
     plot(
         tstate.structure.graph, names = names, method = :stress,
-        fontsize = 5, nodeshape = :hexagon, nodecolor = groups,
+        fontsize = 3, nodesize = 0.3, nodeshape = :circle, nodecolor = groups,
         self_edge_size = 0 # hide self-to-self nodes
     ) # see https://docs.juliaplots.org/stable/generated/graph_attributes/
 end
 
-plot_dependency_graph(pt.ssys)
+plot_dependency_graph(Ms)
