791ea05b30015f431d595bfd8fb196ecb4033526
Herman Sletmoen <hermansletmoen@gmail.com>
2025-11-14 12:14:08 +0100

    Use symbolic Jacobian by default

--- src/solve.jl
+++ src/solve.jl
@@ -106,7 +106,7 @@ end
 """
     CosmologyProblem(
         M::System, pars::Dict, shoot_pars = Dict(), shoot_conditions = [];
-        ivspan = (1e-6, 100.0), bg = true, pt = true, spline = true, debug = false, fully_determined = true, jac = false, kwargs...
+        ivspan = (1e-6, 100.0), bg = true, pt = true, spline = true, debug = false, fully_determined = true, jac = true, kwargs...
     )
 
 Create a numerical cosmological problem from the model `M` with parameters `pars`.
@@ -118,7 +118,7 @@ If `spline` is a `Vector`, it rather decides which (unknown and observed) variab
 """
 function CosmologyProblem(
     M::System, pars::Dict, shoot_pars = Dict(), shoot_conditions = [];
-    ivspan = (1e-6, 100.0), bg = true, pt = true, spline = true, debug = false, fully_determined = true, jac = false, kwargs... # TODO: restore jac = true, not working with massive neutrinos?
+    ivspan = (1e-6, 100.0), bg = true, pt = true, spline = true, debug = false, fully_determined = true, jac = true, kwargs...
 )
     pars = merge(pars, shoot_pars) # save full dictionary for constructor
     parsk = merge(pars, Dict(:k => NaN)) # k is unused, but must be set
